9fb200baab6fb2fd768ee26779e98f9f441d11c6
syu <>
2009-09-18 12:24:16 +0000

    Run on reco/AOD data

--- MyObjectCounter/test/312_eiko.py
+++ MyObjectCounter/test/312_eiko.py
@@ -1,17 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process("Analysis")
-#process = cms.Process("PAT")
+
+process = cms.Process("PAT")
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
-#process.load("Configuration.StandardSequences.Geometry_cff")
-#process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-#process.GlobalTag.globaltag = cms.string('MC_31X_V3::All')
-#process.load("Configuration.StandardSequences.MagneticField_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('MC_31X_V3::All')
+process.load("Configuration.StandardSequences.MagneticField_cff")
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(-1)
     )
 
 process.source = cms.Source("PoolSource",
@@ -19,18 +19,19 @@ process.source = cms.Source("PoolSource",
 #    'file:/mc/fakeev/SingleElectronPt100.root'
 #     'file:/mc/fakeev/PATuple/PAT_nocheckcharge_pt20.root'
 #    'file:/mc/Photon-Jet/PATuple/QCD_photon_test.root'
-     'rfio:/castor/cern.ch/user/g/gaultney/SkimsTest/test.root'
+#     'rfio:/castor/cern.ch/user/g/gaultney/SkimsTest/test.root'
+    'file:/home/yunju/CMSSW/CMSSW_3_1_1/src/Photon_Pargun_0906/NoAnti_SinglePhotonFlatPt90_eta06_906.root'
     )
                             )
 
-#process.load("PhysicsTools.PatAlgos.patSequences_cff")
+process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
 process.MyObjectCounter = cms.EDAnalyzer("MyObjectCounter",
         muoLabel = cms.InputTag( "cleanLayer1Muons" ),
         eleLabel = cms.InputTag( "cleanLayer1Electrons" ),
         phoLabel = cms.InputTag( "cleanLayer1Photons" ),
         triggerEvent = cms.InputTag( "patTriggerEvent" ),
-        trigger      = cms.InputTag( "patTrigger")                                         
+        trigger      = cms.InputTag( "patTrigger"),                  matcherName   = cms.string( "myphotonTriggerL1Match")                                 
  )
 process.MyObjectCounter.dumpHEP = cms.untracked.bool(False);
 
@@ -42,7 +43,7 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 process.TFileService = cms.Service("TFileService", fileName = cms.string('results_eiko.root'))
 
-process.p = cms.Path(#process.patDefaultSequence*
+process.p = cms.Path(process.patDefaultSequence*
                      process.MyObjectCounter)
 
 
