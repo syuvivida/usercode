461a444b8e60a71d9e63888cc5225a7d753c8d23
syu <>
2009-10-06 13:08:02 +0000

    scripts for making plots of baur/pythia MC

--- scripts/read_Wg_mass.C
+++ scripts/read_Wg_mass.C
@@ -5,7 +5,6 @@
 #include <TH2.h>
 #include <TFile.h>
 
-using namespace std;
 
 double ClusterMass(TLorentzVector& gamma, TLorentzVector& electron,
 		   TLorentzVector& neutrino)
@@ -22,7 +21,9 @@ double ClusterMass(TLorentzVector& gamma, TLorentzVector& electron,
 }
 
 
-void read_Wg(std::string inputfile, double totalxsec, int baur=0)
+using namespace std;
+// here, only leading photon is required
+void read_Wg_mass(std::string inputfile, double totalxsec, int baur=0)
 {
   // Booking histograms
   int ntotal=0;
@@ -40,13 +41,8 @@ void read_Wg(std::string inputfile, double totalxsec, int baur=0)
   float dRmax=10.0;
 
   char name[300];
-  sprintf(name,"%s%s",inputfile.data(),"_histo.root");  
+  sprintf(name,"%s%s",inputfile.data(),"_masshisto.root");  
   TFile* outFile = new TFile(name,"recreate");
-
-  TH1F* h_diff = new TH1F("h_diff","P_{T}(#gamma^{ISR})-P_{T}(#gamma^{FSR})",
-			  100,-100,100);
-  TH1F* h_diff2 = new TH1F("h_diff2","P_{T}(#gamma^{ISR})-P_{T}(#gamma^{FSR})",
-			  100,-100,100);
 
   TH1F* h_dR = new TH1F("h_dR","#Delta R (lepton,#gamma)",ndRbin,dRmin,dRmax);
   TH1F* h_mW = new TH1F("h_mW","Mass of W boson",netbin,etmin,etmax);  
@@ -98,53 +94,6 @@ void read_Wg(std::string inputfile, double totalxsec, int baur=0)
 			     netbin,etmin,etmax);
   TH1F* hFSR_gpta = new TH1F("hFSR_gpta","FSR p_{T}(#gamma) after cuts",
 			     netbin,etmin,etmax);
-
-  // after requiring only one leading photon
-
-  
-
-  TH1F* hlead_dR = new TH1F("hlead_dR","#Delta R (lepton,#gamma)",ndRbin,dRmin,dRmax);
-  TH1F* hlead_mW = new TH1F("hlead_mW","Mass of W boson",netbin,etmin,etmax);  
-  TH1F* hlead_mWg = new TH1F("hlead_mWg","Mass of W boson + photon",netbin,etmin,etmax);
-  TH2F* hlead_mW3 = new TH2F("hlead_mW3","M(W#gamma) vs M(W)",netbin,etmin,etmax,netbin,etmin,etmax);
-  TH1F* hlead_dR2 = new TH1F("hlead_dR2","#Delta R (lepton,#gamma)",ndRbin,dRmin,dRmax);
-  TH1F* hlead_mW2 = new TH1F("hlead_mW2","Mass of W boson",netbin,etmin,etmax);  
-  TH1F* hlead_mWg2 = new TH1F("hlead_mWg2","Mass of W boson + photon",netbin,etmin,etmax);
-  TH2F* hlead_mW32 = new TH2F("hlead_mW32","M(W#gamma) vs M(W)",netbin,etmin,etmax,netbin,etmin,etmax);
-
-  TH1F* hleadFSR_dR = new TH1F("hleadFSR_dR","#Delta R (lepton,#gamma)",ndRbin,dRmin,dRmax);
-  TH1F* hleadFSR_mW = new TH1F("hleadFSR_mW","Mass of W boson",netbin,etmin,etmax);  
-  TH1F* hleadFSR_mWg = new TH1F("hleadFSR_mWg","Mass of W boson + photon",netbin,etmin,etmax);
-  TH2F* hleadFSR_mW3 = new TH2F("hleadFSR_mW3","M(W#gamma) vs M(W)",netbin,etmin,etmax,netbin,etmin,etmax);
-  TH1F* hleadFSR_dR2 = new TH1F("hleadFSR_dR2","#Delta R (lepton,#gamma)",ndRbin,dRmin,dRmax);
-  TH1F* hleadFSR_mW2 = new TH1F("hleadFSR_mW2","Mass of W boson",netbin,etmin,etmax);  
-  TH1F* hleadFSR_mWg2 = new TH1F("hleadFSR_mWg2","Mass of W boson + photon",netbin,etmin,etmax);
-  TH2F* hleadFSR_mW32 = new TH2F("hleadhFSR_mW32","M(W#gamma) vs M(W)",netbin,etmin,etmax,netbin,etmin,etmax);
-
-  TH1F* hleadISR_dR = new TH1F("hleadISR_dR","#Delta R (lepton,#gamma)",ndRbin,dRmin,dRmax);
-  TH1F* hleadISR_mW = new TH1F("hleadISR_mW","Mass of W boson",netbin,etmin,etmax);  
-  TH1F* hleadISR_mWg = new TH1F("hleadISR_mWg","Mass of W boson + photon",netbin,etmin,etmax);
-  TH2F* hleadISR_mW3 = new TH2F("hleadISR_mW3","M(W#gamma) vs M(W)",netbin,etmin,etmax,netbin,etmin,etmax);
-  TH1F* hleadISR_dR2 = new TH1F("hleadISR_dR2","#Delta R (lepton,#gamma)",ndRbin,dRmin,dRmax);
-  TH1F* hleadISR_mW2 = new TH1F("hleadISR_mW2","Mass of W boson",netbin,etmin,etmax);  
-  TH1F* hleadISR_mWg2 = new TH1F("hleadISR_mWg2","Mass of W boson + photon",netbin,etmin,etmax);
-  TH2F* hleadISR_mW32 = new TH2F("hleadISR_mW32","M(W#gamma) vs M(W)",netbin,etmin,etmax,netbin,etmin,etmax);
-
-
-  TH1F* hlead_gptb = new TH1F("hlead_gptb","p_{T}(#gamma) before cuts",netbin,etmin,etmax);
-  TH1F* hlead_gpta = new TH1F("hlead_gpta","p_{T}(#gamma) after cuts",netbin,etmin,etmax);
-  TH1F* hlead_lptb = new TH1F("hlead_lptb","p_{T}(e) before cuts",netbin,etmin,etmax);
-  TH1F* hlead_lpta = new TH1F("hlead_lpta","p_{T}(e) after cuts",netbin,etmin,etmax);
-  TH1F* hlead_nptb = new TH1F("hlead_nptb","p_{T}(#nu) before cuts",netbin,etmin,etmax);
-  TH1F* hlead_npta = new TH1F("hlead_npta","p_{T}(#nu) after cuts",netbin,etmin,etmax);
-
-  TH1F* hlead_getab = new TH1F("hlead_getab","#eta(#gamma) before cuts",netabin,etamin,etamax);
-  TH1F* hlead_getaa = new TH1F("hlead_getaa","#eta(#gamma) after cuts",netabin,etamin,etamax);
-  TH1F* hlead_letab = new TH1F("hlead_letab","#eta(e) before cuts",netabin,etamin,etamax);
-  TH1F* hlead_letaa = new TH1F("hlead_letaa","#eta(e) after cuts",netabin,etamin,etamax);
-  TH1F* hlead_netab = new TH1F("hlead_netab","#eta(#nu) before cuts",netabin,etamin,etamax);
-  TH1F* hlead_netaa = new TH1F("hlead_netaa","#eta(#nu) after cuts",netabin,etamin,etamax);
-
   
 
   TH1F* hmT_FSRb = new TH1F("hmT_FSRb","M_{T}(W) before cuts", netbin, etmin, etmax);
@@ -187,25 +136,28 @@ void read_Wg(std::string inputfile, double totalxsec, int baur=0)
   while(!fin.eof()){
     ntotal ++;
     if(ntotal%100==0)cout << "ntotal = " << ntotal << endl;
-    TLorentzVector gamma(cl[0],cl[1],cl[2],cl[3]);
+    TLorentzVector gamma1(cl[0],cl[1],cl[2],cl[3]);
     TLorentzVector clepton(cl[4],cl[5],cl[6],cl[7]);
     TLorentzVector nlepton(cl[8],cl[9],cl[10],cl[11]);
-    TLorentzVector gamma_FSR(cl[12],cl[13],cl[14],cl[15]);
+    TLorentzVector gamma2(cl[12],cl[13],cl[14],cl[15]);
 
-    double weight = baur==1? cl[NCOL] : 1.0;
-    
+    double weight = 1.0;
+
+
+    TLorentzVector gamma_lead = gamma1.Et() > gamma2.Et()? 
+      gamma1: gamma2;
+
+    double mT3 = ClusterMass(gamma_lead,clepton,nlepton);
+
+    bool hasFSR= mT3 < 90.0;
+    bool hasISR= mT3 > 90.0;
+
     double mW = (clepton+nlepton).M();
-    double mWg= (clepton+nlepton+gamma).M();
-    double dR = gamma.DeltaR(clepton);
-    double gpt  =gamma.Pt();
-    double geta =gamma.Eta();
+    double mWg= (clepton+nlepton+gamma_lead).M();
+    double dR = gamma_lead.DeltaR(clepton);
+    double gpt  =gamma_lead.Pt();
+    double geta =gamma_lead.Eta();
     
-
-    double mWg_FSR  = (clepton+nlepton+gamma_FSR).M();
-    double dR_FSR   = gamma_FSR.DeltaR(clepton);
-    double gpt_FSR  = gamma_FSR.Pt();
-    double geta_FSR = gamma_FSR.Eta();
-
 
     TLorentzVector met_lv(nlepton.Px(),                    
 			  nlepton.Py(),
@@ -218,45 +170,26 @@ void read_Wg(std::string inputfile, double totalxsec, int baur=0)
 			  0,
 			  clepton.Pt());
 
-    TLorentzVector gISR_lv(gamma.Px(),
-			gamma.Py(),
+    TLorentzVector g_lv(gamma_lead.Px(),
+			gamma_lead.Py(),
 			0,
-			gamma.Pt());
+			gamma_lead.Pt());
 
-    TLorentzVector gFSR_lv(gamma_FSR.Px(),
-			   gamma_FSR.Py(),
-			   0,
-			   gamma_FSR.Pt());
-
-
-    
 
     double mT = (met_lv+lep_lv).M();
-//     double mT3_ISR = (met_lv+lep_lv + gISR_lv).M();
-//     double mT3_FSR = (met_lv+lep_lv + gFSR_lv).M();
-
-    double mT3_ISR = ClusterMass(gamma,clepton,nlepton);
-    double mT3_FSR = ClusterMass(gamma_FSR,clepton,nlepton);
-
 
     double lpt  =clepton.Pt();
     double leta =clepton.Eta();
     double npt  =nlepton.Pt();
     double neta =nlepton.Eta();
 
-    bool leadIsISR = gpt > gpt_FSR;
-    bool leadIsFSR = gpt_FSR >gpt;
-
     for(int j=0;j<NCOL;j++)
       fin >> cl[j];
 
-    bool hasFSR= gamma_FSR.E()>1e-6;
-    bool hasISR= gamma.E()>1e-6;
     
     if((!hasFSR && !hasISR) || clepton.E() <1e-6 || nlepton.E()<1e-6)
       continue;
 
-    cout << gpt << "\t" << gpt_FSR << endl;
     
     nsubtotal++;
     
@@ -282,81 +215,40 @@ void read_Wg(std::string inputfile, double totalxsec, int baur=0)
       hISR_gptb->Fill(gpt, weight);
 
       hmT_ISRb->Fill(mT, weight);
-      hmT3_ISRb->Fill(mT3_ISR, weight);
-      hmT3mT_ISRb->Fill(mT,mT3_ISR, weight);
+      hmT3_ISRb->Fill(mT3, weight);
+      hmT3mT_ISRb->Fill(mT,mT3, weight);
       
-      if(leadIsISR){
-	hlead_mW->Fill(mW, weight);
-	hlead_mWg->Fill(mWg, weight);
-	hlead_mW3->Fill(mW,mWg, weight);
-	hlead_dR->Fill(dR, weight);
-
-	hleadISR_mW->Fill(mW, weight);
-	hleadISR_mWg->Fill(mWg, weight);
-	hleadISR_mW3->Fill(mW,mWg, weight);
-	hleadISR_dR->Fill(dR, weight);
-
-	hlead_gptb->Fill(gpt, weight);
-	hlead_lptb->Fill(lpt, weight);
-	hlead_nptb->Fill(npt, weight);
-
-	hlead_getab->Fill(geta, weight);
-	hlead_letab->Fill(leta, weight);
-	hlead_netab->Fill(neta, weight);
-      }
-
     }
 
     if(hasFSR)
       {
 
 	h_mW->Fill(mW, weight);
-	h_mWg->Fill(mWg_FSR, weight);
-	h_mW3->Fill(mW,mWg_FSR, weight);
-	h_dR->Fill(dR_FSR, weight);
+	h_mWg->Fill(mWg, weight);
+	h_mW3->Fill(mW,mWg, weight);
+	h_dR->Fill(dR, weight);
 
 	hFSR_mW->Fill(mW, weight);
-	hFSR_mWg->Fill(mWg_FSR, weight);
-	hFSR_mW3->Fill(mW,mWg_FSR, weight);
-	hFSR_dR->Fill(dR_FSR, weight);
+	hFSR_mWg->Fill(mWg, weight);
+	hFSR_mW3->Fill(mW,mWg, weight);
+	hFSR_dR->Fill(dR, weight);
 
-	h_gptb->Fill(gpt_FSR, weight);
+	h_gptb->Fill(gpt, weight);
 	h_lptb->Fill(lpt, weight);
 	h_nptb->Fill(npt, weight);
 
-	h_getab->Fill(geta_FSR, weight);
+	h_getab->Fill(geta, weight);
 	h_letab->Fill(leta, weight);
 	h_netab->Fill(neta, weight);
 
-	hFSR_gptb->Fill(gpt_FSR, weight);
+	hFSR_gptb->Fill(gpt, weight);
 
 	hmT_FSRb->Fill(mT, weight);
-	hmT3_FSRb->Fill(mT3_FSR, weight);
-	hmT3mT_FSRb->Fill(mT,mT3_FSR, weight);
-
-	if(leadIsFSR){
-	  hlead_mW->Fill(mW, weight);
-	  hlead_mWg->Fill(mWg_FSR, weight);
-	  hlead_mW3->Fill(mW,mWg_FSR, weight);
-	  hlead_dR->Fill(dR_FSR, weight);
-
-	  hleadFSR_mW->Fill(mW, weight);
-	  hleadFSR_mWg->Fill(mWg_FSR, weight);
-	  hleadFSR_mW3->Fill(mW,mWg_FSR, weight);
-	  hleadFSR_dR->Fill(dR_FSR, weight);
-
-	  hlead_gptb->Fill(gpt_FSR, weight);
-	  hlead_lptb->Fill(lpt, weight);
-	  hlead_nptb->Fill(npt, weight);
-
-	  hlead_getab->Fill(geta_FSR, weight);
-	  hlead_letab->Fill(leta, weight);
-	  hlead_netab->Fill(neta, weight);
-	}
+	hmT3_FSRb->Fill(mT3, weight);
+	hmT3mT_FSRb->Fill(mT,mT3, weight);
 
       }
           
-//     if(hasFSR && hasISR)h_diff->Fill(gpt-gpt_FSR, weight);
 
 //     // muon channel LSP's cuts
 //     bool photonCut_ISR = gpt>10.0 && fabs(geta)<2.7;
@@ -392,9 +284,7 @@ void read_Wg(std::string inputfile, double totalxsec, int baur=0)
 
     // CDF cuts
      bool photonCut_ISR = gpt> 7.0 && dR>0.7;
-     bool photonCut_FSR = gpt_FSR > 7.0 && dR_FSR>0.7;
-     if(!photonCut_ISR && !photonCut_FSR)continue;
-//      if(!photonCut_ISR)continue;
+     if(!photonCut_ISR)continue;
 
 
     npass++;
@@ -415,91 +305,44 @@ void read_Wg(std::string inputfile, double totalxsec, int baur=0)
       h_dR2->Fill(dR, weight);
 
       
-      if(photonCut_ISR){
-	hISR_mW2->Fill(mW, weight);
-	hISR_mWg2->Fill(mWg, weight);
-	hISR_mW32->Fill(mW,mWg, weight);
-	hISR_dR2->Fill(dR, weight);
-	hISR_gpta->Fill(gpt, weight);
-	hmT_ISRa->Fill(mT, weight);
- 	hmT3_ISRa->Fill(mT3_ISR, weight);
-	hmT3mT_ISRa->Fill(mT,mT3_ISR, weight);
-      }
-
-
-      if(leadIsISR && photonCut_ISR){
-	hlead_gpta->Fill(gpt, weight);
-	hlead_lpta->Fill(lpt, weight);
-	hlead_npta->Fill(npt, weight);
-
-	hlead_getaa->Fill(geta, weight);
-	hlead_letaa->Fill(leta, weight);
-	hlead_netaa->Fill(neta, weight);
+      hISR_mW2->Fill(mW, weight);
+      hISR_mWg2->Fill(mWg, weight);
+      hISR_mW32->Fill(mW,mWg, weight);
+      hISR_dR2->Fill(dR, weight);
+      hISR_gpta->Fill(gpt, weight);
+      hmT_ISRa->Fill(mT, weight);
+      hmT3_ISRa->Fill(mT3, weight);
+      hmT3mT_ISRa->Fill(mT,mT3, weight);
     
-
-	hlead_mW2->Fill(mW, weight);
-	hlead_mWg2->Fill(mWg, weight);
-	hlead_mW32->Fill(mW,mWg, weight);
-	hlead_dR2->Fill(dR, weight);
-
-	hleadISR_mW2->Fill(mW, weight);
-	hleadISR_mWg2->Fill(mWg, weight);
-	hleadISR_mW32->Fill(mW,mWg, weight);
-	hleadISR_dR2->Fill(dR, weight);
-      }
 
     }
 
    if(hasFSR)
       {
 	h_mW2->Fill(mW, weight);
-	h_mWg2->Fill(mWg_FSR, weight);
-	h_mW32->Fill(mW,mWg_FSR, weight);
-	h_dR2->Fill(dR_FSR, weight);
+	h_mWg2->Fill(mWg, weight);
+	h_mW32->Fill(mW,mWg, weight);
+	h_dR2->Fill(dR, weight);
 
-	h_gpta->Fill(gpt_FSR, weight);
+	h_gpta->Fill(gpt, weight);
 	h_lpta->Fill(lpt, weight);
 	h_npta->Fill(npt, weight);
 
-	h_getaa->Fill(geta_FSR, weight);
+	h_getaa->Fill(geta, weight);
 	h_letaa->Fill(leta, weight);
 	h_netaa->Fill(neta, weight);
 
-	if(photonCut_FSR){
-	  hFSR_mW2->Fill(mW, weight);
-	  hFSR_mWg2->Fill(mWg_FSR, weight);
-	  hFSR_mW32->Fill(mW,mWg_FSR, weight);
-	  hFSR_dR2->Fill(dR_FSR, weight);
-	  hFSR_gpta->Fill(gpt_FSR, weight);
-	  hmT_FSRa->Fill(mT, weight);
-	  hmT3_FSRa->Fill(mT3_FSR, weight);
-	  hmT3mT_FSRa->Fill(mT,mT3_FSR, weight);
-	}
-
-	if(leadIsFSR && photonCut_FSR){
-	  hlead_mW2->Fill(mW, weight);
-	  hlead_mWg2->Fill(mWg_FSR, weight);
-	  hlead_mW32->Fill(mW,mWg_FSR, weight);
-	  hlead_dR2->Fill(dR_FSR, weight);
-
-	  hleadFSR_mW2->Fill(mW, weight);
-	  hleadFSR_mWg2->Fill(mWg_FSR, weight);
-	  hleadFSR_mW32->Fill(mW,mWg_FSR, weight);
-	  hleadFSR_dR2->Fill(dR_FSR, weight);
-
-	  hlead_gpta->Fill(gpt_FSR, weight);
-	  hlead_lpta->Fill(lpt, weight);
-	  hlead_npta->Fill(npt, weight);
-
-	  hlead_getaa->Fill(geta_FSR, weight);
-	  hlead_letaa->Fill(leta, weight);
-	  hlead_netaa->Fill(neta, weight);
-
-	}
+	hFSR_mW2->Fill(mW, weight);
+	hFSR_mWg2->Fill(mWg, weight);
+	hFSR_mW32->Fill(mW,mWg, weight);
+	hFSR_dR2->Fill(dR, weight);
+	hFSR_gpta->Fill(gpt, weight);
+	hmT_FSRa->Fill(mT, weight);
+	hmT3_FSRa->Fill(mT3, weight);
+	hmT3mT_FSRa->Fill(mT,mT3, weight);
+      
 
       }
-
-   if(hasFSR && hasISR)h_diff2->Fill(gpt-gpt_FSR, weight);
 
 
   }
@@ -515,9 +358,6 @@ void read_Wg(std::string inputfile, double totalxsec, int baur=0)
   double err = sqrt( (1-eff)*eff/ntotal);
   cout << "filter eff = " << eff << " +- " << err << endl;
 
-//   sprintf(name,inputfile.data(),"_xsec");
-//   fin.open(name);
-//   fin.close();
   cout << "totalxsec = " << totalxsec << endl;
   double xsec = totalxsec*eff*1e-3/1e-12;
   double xsec_err = totalxsec*err*1e-3/1e-12;
