c9ab31c168f57fb3d5a58edb62024f0dd23907c1
syu <>
2009-12-06 22:39:18 +0000

    Common Algorithm with template functions

---
+++ MyObjectCounter/header/MyAlg.hh
@@ -0,0 +1,281 @@
+#ifndef MYALG_HH
+#define MYALG_HH
+
+#include <iostream>
+#include <map>
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticleFwd.h"
+#include "DataFormats/EgammaCandidates/interface/Photon.h"
+#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectronFwd.h"
+
+#include "DataFormats/L1Trigger/interface/L1EmParticle.h"
+#include "DataFormats/L1Trigger/interface/L1EmParticleFwd.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/HLTReco/interface/TriggerEvent.h"
+#include "DataFormats/HLTReco/interface/TriggerTypeDefs.h"
+#include "FWCore/Framework/interface/TriggerNames.h"
+#include "DataFormats/Math/interface/deltaR.h"
+
+#include "syu/MyObjectCounter/header/format.hh" 
+#include "syu/MyObjectCounter/header/trigformat.hh"
+
+
+using namespace std;
+using namespace edm;
+using namespace reco;
+using namespace trigger;
+using namespace math;
+using namespace ROOT;
+
+
+template <class T>
+struct myContainer
+{
+  typedef typename std::vector<T> container;
+  typedef typename container::const_iterator myIter;
+};
+
+template <class T>
+struct partEtMap
+{
+  typedef typename std::map< float, typename myContainer<T>::myIter, std::greater<float> > Type;
+};
+
+template <class T>
+struct partL1Map
+{
+  typedef typename std::map< typename myContainer<T>::myIter, 
+			     l1extra::L1EmParticleCollection::const_iterator > Type;
+};
+
+
+template <class T>
+struct partL3Map
+{
+  typedef typename std::map< typename myContainer<T>::myIter, trigger::TriggerObject > Type;
+  
+};
+
+class MyAlg {
+//-----------------------------------------------------------------------------
+//  methods
+//-----------------------------------------------------------------------------
+public:
+  MyAlg(edm::ParameterSet const & parameters);
+  MyAlg(){}
+  ~MyAlg();
+
+  MyAlg& operator=( const MyAlg& original);
+
+  void init(const edm::Event & event,
+	    bool doPho,
+	    bool doEle,
+	    bool doGen,
+	    bool doHLT);
+
+  void getHandles(const edm::Event  & event, 
+		  bool doPho,
+		  bool doEle,
+		  bool doGen,
+		  bool doHLT);
+
+  void turnOnHLTBit(std::string trgPath, int trgCode);
+
+  void dumpGenInfo(const edm::Event&); 
+
+  void print();
+
+  int        getThisEventTriggerBit(){return _event_trigger;}
+  void       sortGenParticles(); // with a PDG code cut
+  template<class T> void sortParticles(const edm::Handle<typename myContainer<T>::container >& handle,
+				       typename partEtMap<T>::Type& etmap);
+
+  template <class T> void matchPartToL1(typename partEtMap<T>::Type& etmap, 
+					typename partL1Map<T>::Type& mymap );
+
+  template <class T> void matchPartToL3(std::string trgPath, std::string tag,
+					typename partEtMap<T>::Type& etmap,
+					typename partL3Map<T>::Type& mymap);
+
+  partEtMap<reco::Photon>::Type      getPhoEtMap(){return _phoEtMap;}
+  partEtMap<reco::GsfElectron>::Type getEleEtMap(){return _eleEtMap;}
+  partEtMap<reco::GenParticle>::Type getGenEtMap(){return _genEtMap;}
+  
+private: 
+
+   // Keep a version of the parameter set in question
+  edm::Handle<reco::PhotonCollection>          _phoHandle;
+  edm::Handle<reco::GsfElectronCollection>     _eleHandle;
+  edm::Handle<reco::GenParticleCollection>     _genHandle;
+  edm::Handle<l1extra::L1EmParticleCollection> _l1EmNonIsoHandle;
+  edm::Handle<l1extra::L1EmParticleCollection> _l1EmIsoHandle;
+  edm::Handle<trigger::TriggerEvent>           _trgEventHandle;
+  edm::Handle<edm::TriggerResults>             _trgResultsHandle;
+
+  partEtMap<reco::Photon>::Type          _phoEtMap;
+  partEtMap<reco::GsfElectron>::Type     _eleEtMap;
+  partEtMap<reco::GenParticle>::Type     _genEtMap;
+
+  edm::ParameterSet         _parameters;
+  bool _dumpHEP;
+  int  _pdgCode;
+  double _deltaRMax;
+  int _event_trigger;
+
+};
+
+// === inline definition of template functions (has to be in the header files)
+//
+
+template < class T> void MyAlg::sortParticles(const edm::Handle< typename myContainer<T>::container >& handle,  
+					      typename partEtMap<T>::Type& sortedEtMap)
+{                                                                                                        
+  sortedEtMap.clear();                                                                                           
+  if(!handle.isValid())return;                                                                                   
+  typename myContainer<T>::myIter it_part;                                                                      
+                                                                                                  
+  for ( it_part = handle->begin(); it_part != handle->end(); it_part++){
+    float et = it_part->pt();                                                                                   
+    sortedEtMap.insert(std::pair<float,typename myContainer<T>::myIter>(et,it_part));                           
+  } 
+  return;
+}
+
+
+template <class T> void MyAlg::matchPartToL1(typename partEtMap<T>::Type& etmap,
+					     typename partL1Map<T>::Type& mymap)
+{
+  mymap.clear();
+
+  if(!_l1EmIsoHandle.isValid() 
+     && !_l1EmNonIsoHandle.isValid())return;
+
+  typedef typename partEtMap<T>::Type::iterator mapIter;
+ 
+  for(mapIter it_part=etmap.begin(); 
+      it_part!= etmap.end(); ++it_part)
+    {
+
+      float ptMax = 0;
+      std::vector<l1extra::L1EmParticle>::const_iterator maxPtIter;
+      bool hasMatch = false;
+
+      for( std::vector<l1extra::L1EmParticle>::const_iterator it_l1 = 
+	   _l1EmIsoHandle->begin(); 
+	   it_l1 != _l1EmIsoHandle->end(); it_l1++ ) {
+    
+	float deltaR = reco::deltaR(it_l1->eta(), it_l1->phi(),
+				    it_part->second->eta(), 
+				    it_part->second->phi());
+	
+	float thisL1Pt = it_l1->pt();
+	
+	if (deltaR < _deltaRMax && thisL1Pt > ptMax ) {
+	  
+	  ptMax = thisL1Pt;
+	  maxPtIter = it_l1;
+	  hasMatch = true;
+      }
+
+    } // end of L1 isolated trigger objects
+
+
+    for( std::vector<l1extra::L1EmParticle>::const_iterator it_l1 = 
+	  _l1EmNonIsoHandle->begin(); 
+	 it_l1 != _l1EmNonIsoHandle->end(); it_l1++ ) {
+    
+      float deltaR = reco::deltaR(it_l1->eta(), it_l1->phi(),
+				  it_part->second->eta(), it_part->second->phi());
+
+      float thisL1Pt = it_l1->pt();
+
+      if (deltaR < _deltaRMax && thisL1Pt > ptMax ) {
+	
+	ptMax = thisL1Pt;
+	maxPtIter = it_l1;
+	hasMatch = true;
+      }
+
+    } // end of L1 non-isolated trigger objects
+
+    
+    if(hasMatch)
+      mymap.insert(std::pair<typename myContainer<T>::myIter,
+		   l1extra::L1EmParticleCollection::const_iterator>(it_part->second,maxPtIter));    
+  } // end of loop over photons
+
+  return;
+
+
+
+
+}
+
+
+//_____________________________________________________________________________
+//
+template <class T> void MyAlg::matchPartToL3(std::string trgPath,
+					     std::string tag,
+					     typename partEtMap<T>::Type& etmap,
+					     typename partL3Map<T>::Type& mymap)
+
+{
+  mymap.clear();
+
+  // check for HLT objects
+  if(!_trgEventHandle.isValid())return;
+
+
+  const trigger::TriggerObjectCollection& 
+    TOC(_trgEventHandle->getObjects());
+  const edm::InputTag myLastFilter = edm::InputTag(trgPath,"",tag);
+  trigger::size_type thisFilterIndex = 
+    _trgEventHandle->filterIndex(myLastFilter);
+  if ( thisFilterIndex >= _trgEventHandle->sizeFilters())return;
+  const trigger::Keys& keys( _trgEventHandle->filterKeys(thisFilterIndex));
+
+  typedef typename partEtMap<T>::Type::iterator mapIter;
+  for(mapIter it_part=etmap.begin(); 
+      it_part!= etmap.end(); ++it_part)
+    {
+ 
+      float ptMax = 0;
+      trigger::TriggerObject maxPtIter;
+      bool hasMatch = false;
+
+      for ( unsigned int hlto = 0; hlto < keys.size(); hlto++ ) {
+	size_type hltf = keys[hlto];
+	const trigger::TriggerObject L3obj(TOC[hltf]);
+      
+	float deltaR = reco::deltaR(L3obj.eta(), L3obj.phi(),
+				    it_part->second->eta(), it_part->second->phi());
+	float thisL3Pt = L3obj.pt();
+
+	if (deltaR < _deltaRMax && thisL3Pt > ptMax) {
+	  maxPtIter = L3obj;
+	  ptMax = thisL3Pt;
+	  hasMatch = true;
+	}
+      } // end of loop over trigger objects
+
+      if(hasMatch)
+	mymap.insert(std::pair<typename myContainer<T>::myIter,
+		     trigger::TriggerObject>(it_part->second,maxPtIter));    
+
+
+    }// end of loop over photons
+
+  return;
+}
+
+
+
+
+
+
+#endif
---
+++ MyObjectCounter/src/MyAlg.cc
@@ -0,0 +1,198 @@
+
+#include "syu/MyObjectCounter/header/MyAlg.hh" 
+
+
+MyAlg::MyAlg( const edm::ParameterSet & iConfig )  :
+  _parameters( iConfig ), _event_trigger(0)
+{
+  _dumpHEP = iConfig.getUntrackedParameter<bool>("dumpHEP", false);
+  _pdgCode = iConfig.getUntrackedParameter<int>("pdgCode",  22);
+  _deltaRMax  = iConfig.getUntrackedParameter<double>("dR",  0.5);
+}
+
+//_____________________________________________________________________________
+MyAlg::~MyAlg() {
+}
+
+MyAlg& MyAlg::operator=( const MyAlg& original)
+{
+  if ( this == 0 ) {
+    std::cerr << "EXITING: trying to use 'operator=' with an MatrixMeschach for which memory space is not reserved!!!!" << std::endl;
+    std::exception();
+  }
+  
+  if ( this != &original ) {
+    _parameters = original._parameters;
+    _phoHandle   = original._phoHandle;
+    _eleHandle   = original._eleHandle;
+    _genHandle   = original._genHandle;
+    _l1EmNonIsoHandle = original._l1EmNonIsoHandle;
+    _l1EmIsoHandle = original._l1EmIsoHandle;
+    _trgEventHandle = original._trgEventHandle;
+    _trgResultsHandle = original._trgResultsHandle;
+    _phoEtMap = original._phoEtMap;
+    _eleEtMap = original._eleEtMap;
+    _genEtMap = original._genEtMap;
+    _dumpHEP  = original._dumpHEP;
+    _pdgCode  = original._pdgCode;
+    _deltaRMax = original._deltaRMax;
+    _event_trigger = original._event_trigger;
+   
+  }
+   return *this;
+}
+
+void MyAlg::print()
+{
+  std::cout << "dumpHEP = " << _dumpHEP << std::endl;
+  std::cout << "pdgCode = " << _pdgCode << std::endl;
+  std::cout << "deltaRMax = " << _deltaRMax << std::endl;
+  std::cout << "thisEvent_trigger = " << _event_trigger << std::endl;
+}
+
+
+void MyAlg::init(const edm::Event &event,
+		   bool doPho,
+		   bool doEle,
+		   bool doGen,
+		   bool doHLT)
+{
+  _event_trigger = 0;
+  _genEtMap.clear();
+  _phoEtMap.clear();
+  _eleEtMap.clear();
+  getHandles(event, doPho, doEle, doGen, doHLT);
+  if(_dumpHEP)dumpGenInfo(event);
+
+}
+
+void MyAlg::getHandles(const edm::Event  & event,
+		       bool doPho,
+		       bool doEle,
+		       bool doGen,
+		       bool doHLT)
+
+{
+ 
+  if(doPho){
+    edm::InputTag photonName       = _parameters.getParameter<edm::InputTag>("phoLabel"  );
+    event.getByLabel(photonName, _phoHandle);
+    sortParticles<reco::Photon>(_phoHandle, _phoEtMap);
+ 
+  }
+
+  if(doEle){
+    edm::InputTag electronName     = _parameters.getParameter<edm::InputTag>("eleLabel");
+    event.getByLabel(electronName, _eleHandle);
+    sortParticles<reco::GsfElectron>(_eleHandle, _eleEtMap);
+  }
+
+  if(doGen){
+    edm::InputTag genParticlesName = _parameters.getParameter<edm::InputTag>("genLabel");
+    event.getByLabel(genParticlesName, _genHandle);
+    sortGenParticles();
+  }
+
+  if(doHLT){
+    edm::InputTag HLTName          = _parameters.getParameter<edm::InputTag>("HLTLabel");
+    event.getByLabel(HLTName, _trgResultsHandle);
+  }
+
+  const edm::InputTag l1EmNonIsoTag = edm::InputTag("hltL1extraParticles","NonIsolated");
+  const edm::InputTag l1EmIsoTag = edm::InputTag("hltL1extraParticles","Isolated");
+  const edm::InputTag hltsummaryTag = edm::InputTag("hltTriggerSummaryAOD","","HLT");
+
+  event.getByLabel(l1EmNonIsoTag, _l1EmNonIsoHandle);
+  event.getByLabel(l1EmIsoTag, _l1EmIsoHandle);
+  event.getByLabel(hltsummaryTag, _trgEventHandle);
+
+}
+
+
+void MyAlg::sortGenParticles(){
+
+  _genEtMap.clear();
+  if(!_genHandle.isValid())return;
+  int count = 0;
+  for (reco::GenParticleCollection::const_iterator it_gen = 
+	 _genHandle->begin(); 
+       it_gen!=_genHandle->end() && count < MAX_GENS; it_gen++){
+    if(abs(it_gen->pdgId())!=_pdgCode || it_gen->status()!=1)continue;
+    if(it_gen->pt() < 2.)continue;
+    count ++ ;
+    float et = it_gen->pt();
+    _genEtMap.insert(std::pair<float,reco::GenParticleCollection::const_iterator>(et,it_gen));
+  }
+  return;
+}
+
+
+
+//_____________________________________________________________________________
+void MyAlg::turnOnHLTBit(std::string trgPath, int trgCode)
+{
+  if(!_trgResultsHandle.isValid())return;
+  edm::TriggerNames trgName( *_trgResultsHandle);   
+  int NTrigger = trgName.size();
+  int tempIndex = (unsigned int)trgName.triggerIndex( trgPath); 
+  if(tempIndex < NTrigger && _trgResultsHandle->accept(tempIndex))
+    _event_trigger |= trgCode;
+  return;
+}
+
+
+
+//_____________________________________________________________________________
+// dump generator-level information
+
+
+void MyAlg::dumpGenInfo(const edm::Event& iEvent)
+{
+
+  if(!_genHandle.isValid())return;
+
+  std::cout << "============================ " << 
+    "Run " <<  iEvent.id().run() << " Evt " <<  iEvent.id().event() <<
+    " ============================= " << std::endl << std::endl;
+
+  int genIndex = 0;
+
+  printf("GenIndex  ");
+  printf("PDG  ");
+  printf("Status ");
+  printf("Mother PID");
+  printf("   ");
+  printf("Mass ");
+  printf("Energy ");
+  printf("Pt");
+  printf("\n");
+
+
+  for( reco::GenParticleCollection::const_iterator it_gen = _genHandle->begin(); 
+       it_gen != _genHandle->end(); it_gen++ ) {
+
+    printf("%4i", genIndex);
+    printf("%7i", it_gen->pdgId());
+    printf("%6i", it_gen->status());
+    if(it_gen->mother())
+      printf("%7i", it_gen->mother()->pdgId());
+    else
+      printf("%7i", -1);
+    printf("   ");
+    printf("%9.3f",it_gen->p4().mass());
+    printf("%9.3f",it_gen->energy());
+    printf("%9.3f",it_gen->pt());
+    printf("%9.3f",it_gen->vx());
+    printf("%9.3f",it_gen->vy());
+    printf("%9.3f",it_gen->vz());
+    printf("\n");
+    genIndex ++;
+  }
+
+  std::cout << std::endl 
+	    << "==========================================================="
+            << "===============" 
+	    << std::endl;
+  return;
+
+}
