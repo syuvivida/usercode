893cd53877daa81b4ea40be91f53789c05ba15ba
syu <>
2010-03-17 21:14:21 +0000

    selections for data comparison

--- scripts/datamc_comparison/dataSelection.h
+++ scripts/datamc_comparison/dataSelection.h
@@ -1,30 +1,31 @@
 #include <TCut.h>
 
 TCut basicCut    = "vtxIsFake==0 && vtxNdof >= 4"
-  //  " && ((PHOLEAD_isEB==1 && PHOLEAD_isEE==0 && !(abs(PHOLEAD_eMax/PHOLEAD_e3x3-1)<0.1)) || (PHOLEAD_isEB==0 && PHOLEAD_isEE==1))" 
-   " && nHfTowersP>=1 && nHfTowersN>=1"
-  //  " && PHOLEAD_rawEnergy*sin( (2*atan(exp(-PHOLEAD_scEta)))) > 2.0"
+  " && ((PHOLEAD_isEB==1 && PHOLEAD_isEE==0 && !(abs(PHOLEAD_eMax/PHOLEAD_e3x3-1)<0.1)) || (PHOLEAD_isEB==0 && PHOLEAD_isEE==1))" 
+  " && PHOLEAD_rawEnergy*sin( (2*atan(exp(-PHOLEAD_scEta)))) > 2.0"
   ;
 
-TCut fiducialCut = "PHOLEAD_isEBEEGap==0 && PHOLEAD_isEBGap==0"
-  " && PHOLEAD_isEEGap==0"
+TCut fiducialCut = //"PHOLEAD_isEBEEGap==0 && PHOLEAD_isEBGap==0"
+  //  " && PHOLEAD_isEEGap==0"
   //  " && abs(PHOLEAD_scEta) < 2.5"
+  "abs(PHOLEAD_scEta) < 2.5 &&"
+  "( abs(PHOLEAD_scEta) < 1.4442 || abs(PHOLEAD_scEta) > 1.566)"
   ;
 
 TCut myCut       = basicCut + fiducialCut;
-
 TCut looseCut    = basicCut;
 
 TCut barrelCut   = "PHOLEAD_isEB==1 && PHOLEAD_isEE==0";
 TCut endcapCut   = "PHOLEAD_isEB==0 && PHOLEAD_isEE==1";
 
+
 TCut runCut = "run == 123596 ||"
   "run == 123615 ||"
-  "run == 123732 ||"
+  //  "run == 123732 ||"
   "run == 123818 ||"
   "run == 123906 ||"
-  "run == 123908 ||"
-  "run == 123909 ||"
+  //  "run == 123908 ||"
+  //  "run == 123909 ||"
   "run == 124009 ||"
   "run == 124020 ||"
   "run == 124022 ||"
