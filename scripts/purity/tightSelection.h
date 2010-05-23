8357c8fa7fddc1008142bcbbd06b8c996c765e29
syu <>
2010-05-23 15:13:47 +0000

    cuts clean-up

--- scripts/purity/tightSelection.h
+++ scripts/purity/tightSelection.h
@@ -22,42 +22,15 @@ TCut rsCut = "( pt > 15.0 && abs(eta)<1.45 && hadronicOverEm <  0.05 && sigmaIet
   "(pt > 15.0 && abs(eta) > 1.7 && abs(eta) < 2.5 && hadronicOverEm <  0.05 && abs(ESRatio) > 0.1)";
 
 
-TCut RSCut_EB = "pt > 15.0 && abs(eta)<1.45 &&"
-  " hadronicOverEm <  0.05 && "
-  " sigmaIetaIeta < 0.01";
-
-TCut RSCut_EE = "pt > 15.0 && abs(eta) > 1.7 && abs(eta) < 2.5 && "
-  //  "(ecalRecHitSumEtConeDR04+hcalTowerSumEtConeDR04+trkSumPtHollowConeDR04) < 1.45 &&"
-  " hadronicOverEm <  0.05 && "
-  " abs(ESRatio) > 0.1";
-
-
 TCut hardScatterCut = "isGenMatched && genMomId==22 && genCalIsoDR04 < 5.0"; 
 TCut fragCut = "isGenMatched && abs(genMomId)<22 && genNSiblings>2 && genCalIsoDR04 < 5.0";
 TCut decayCut = "(isGenMatched && abs(genMomId)>50) || !isGenMatched"; 
 
 TCut IDCut = rsCut;
 
-TCut sigCut = "isGenMatched && abs(genMomId) <= 22 && genCalIsoDR04 < 10.0"; 
+TCut sigCut = "isGenMatched && abs(genMomId) <= 22 && genCalIsoDR04 < 5.0"; 
 TCut bkgCut = !sigCut;
 
-TCut sigCut1 = "isGenMatched && abs(genMomId)==22 && genCalIsoDR04 < 10.0";
-TCut sigCut2 = "isGenMatched && abs(genMomId)<= 22 && genCalIsoDR04 < 10.0";
-
-
-/*
-TCut sigCut1 = hardScatterCut;
-TCut sigCut2 = "isGenMatched && abs(genMomId)<= 22";
-TCut sigCut3 = "isGenMatched && genMomId==22 && genCalIsoDR04 < 5.0";
-TCut sigCut4 = "isGenMatched && abs(genMomId)<=22 && genCalIsoDR04 < 5.0";
-TCut sigCut5 = "isGenMatched && genCalIsoDR04 < 5.0";
-*/
-
-TCut simpleCut = "hadronicOverEm < 0.15&& abs(eta)<2.5 && pt>17"; 
-
-TCut etaCut1 = "isEB && !isEE";
-
-TCut etaCut2 = "isEE && !isEB";
 
 
 
