c725bcc2b1987a8e7c53c63cd4d02f879d6cff35
syu <>
2010-05-26 20:22:46 +0000

    Small modifition and bug fix of Rong-Shyang's semi-unbinned fitter

---
+++ scripts/purity/ext-mL_fit_unbinned.C
@@ -0,0 +1,508 @@
+#include "TH2.h"
+#include "TStyle.h"
+#include "TCanvas.h"
+#include "TF1.h"
+#include <fstream>
+#include <iostream>
+#include <TMinuit.h>
+#include "vector.h"
+#include <TMath.h>
+#include "TVirtualFitter.h"
+#include "TFile.h"
+
+
+using namespace std;
+
+#define NPAR 2
+#define datapoint 1890
+
+TF1* fchi2;
+//TH1F* h1;
+TF1* bsFin;
+
+// Remember to change the values of xh and xl in all_tree.C too!!!
+//Double_t xh = 6.5;
+//Double_t xl = 4.5;
+Double_t xl = -1.;
+Double_t xh = 1.;
+Double_t bin_size = 0.05;
+
+const double _two_pi = 2.0 * TMath::Pi();
+Double_t fit_lo_edge = -1.;
+Double_t fit_hi_edge = 1.;
+
+
+vector<Double_t> dataColl;
+vector<Double_t> sigColl;
+vector<Double_t> bkgColl;
+
+vector<Double_t> totalColl;
+vector<Double_t> ctauColl;
+
+vector<Double_t> info;
+vector<Double_t> info_err;
+
+vector<Double_t> Para;
+
+Double_t SigPDFnorm = 0.;
+Double_t BkgPDFnorm = 0.;
+
+//par[0] = fs Jpsi signal fraction;
+//-----mass part-----------------------------------------------------
+//par[1] = g norm; par[2] g1 mean; par[3] g1 width; 
+//par[4] g2 ratio;  par[5] g2 mean; par[6] g2 width;
+//par[7] bg norm; par[8] bg slope;
+
+void fcn(Int_t &npar, Double_t *gin, Double_t &f, Double_t *par, Int_t iflag)
+{
+
+  Double_t Lsum=0.;
+  Double_t Nevt=dataColl.size();
+  Double_t fs = par[0];
+  Double_t fb = par[1];
+
+  double tmppar[12];
+  for(int i=0; i<12; i++){
+    tmppar[i] = Para[i];
+  }
+  for ( int i=0; i<dataColl.size(); i++ ) {
+    //PDF for signal and background
+    Double_t x = dataColl[i];
+    Double_t Ls = exp_conv_norm(&x,tmppar);
+    Double_t Lb = expinv_power_norm(&x,tmppar);
+    //Get Log Likelihood		  
+    Lsum += TMath::Log( (fs*Ls + fb*Lb) / (fs+fb) );
+  }
+  f=2*( -1*Lsum + (fs+fb) - Nevt*TMath::Log(fs+fb) );
+}
+
+void pulltest(int ptbin=15, char EBEE[10]="EB", float input=0.5){
+
+
+  TH1F *h1 = new TH1F("h1","",100,-10., 10.);
+  TH1F *h2 = new TH1F("h2","",3000, 0., 3000);
+
+  h1->SetNdivisions(505,"XY");
+  h2->SetNdivisions(505,"XY");
+
+  int nexp=1000;
+  Double_t Nevt=0.;
+
+  for (int i=0; i<nexp; i++) {
+    Ifit(ptbin,EBEE);
+    Nevt=0.;
+    for ( int ii=0; ii<dataColl.size(); ii++ ) {
+      Nevt += dataColl[ii];
+    }
+    printf("fit purity %2.2f +- %2.2f err with %d events. \n", info[0], info_err[0], Nevt);
+    h1->Fill((info[0]/Nevt-input)/(info_err[0]/Nevt));
+    h2->Fill(info[0]);
+  }    
+
+  TCanvas *c2 = new TCanvas("c2","",1000,500);
+  c2->Divide(2,1);
+  c2->cd(1);
+  char txt[100];
+  sprintf(txt, "(purity-input)/error");
+  h1->SetXTitle(txt);
+  h1->Fit("gaus");
+  h1->Draw();
+  c2->cd(2);
+  sprintf(txt, "fitted signal (input %d)", input*Nevt);
+  h2->SetXTitle(txt);
+  h2->Fit("gaus");
+  h2->GetXaxis()->SetRangeUser(0., Nevt*1.2);
+  if ( input >0.8 )  h2->GetXaxis()->SetRangeUser(0., Nevt*1.4);
+  h2->Draw();  
+  sprintf(txt, "plots/extmLfit_pull_%s_pt%d.pdf", EBEE, ptbin);
+  c2->SaveAs(txt);
+
+  
+}
+
+
+
+//___________________________________________________________________________
+Double_t* Ifit(TH1F* dataInput, TH1F* sigTemplate, TH1F* bkgTemplate, 
+	       int fit_data=1)
+{
+
+  std::string histoName = dataInput->GetName();
+
+  TCanvas *c1 = new TCanvas("HF1", "Histos1", 0, 0, 600, 600);
+  double count=0;
+  dataColl.clear();
+  sigColl.clear();
+  bkgColl.clear();
+
+  totalColl.clear();
+  ctauColl.clear();
+
+  Para.clear();
+
+  Double_t* fitted = new Double_t[4];
+  fitted[0] = 0.;    fitted[1] = 0.;    fitted[2] = 0.;    fitted[3] = 0.;
+
+  sigTemplate->SetLineColor(1);
+  bkgTemplate->SetLineColor(1);
+
+  TH1F *hsum = (TH1F*)sigTemplate->Clone();
+  hsum->Add(bkgTemplate,1);
+  float ntemplate = 1.;
+  if (hsum->Integral()>1.) ntemplate = hsum->Integral();
+  float sigfrac = sigTemplate->Integral()/ntemplate;
+
+  TH1F *hsum_norm = (TH1F*)hsum->Clone();  
+  hsum_norm->Scale(1./hsum->Integral());
+
+  TH1F *hdata = new TH1F();
+  int ndata=0;
+  if ( fit_data==1 ) {
+    hdata = (TH1F*)dataInput->Clone();
+    ndata = hdata->Integral();    
+    for(int ibin=1; ibin<=hdata->GetNbinsX(); ibin++){
+      for(int ipoint=0; ipoint<hdata->GetBinContent(ibin); ipoint++) {
+	dataColl.push_back(hdata->GetBinCenter(ibin));
+      }
+    }
+    ndata = dataColl.size();
+  }
+
+  if(ndata==0) {
+    printf(" ---  no evetns in the fit \n");
+    return fitted;
+  }
+    
+  //test fit the template and get PDFs
+  TCanvas *c10 = new TCanvas("c10","",1000,500);
+  c10->Divide(2,1);
+  c10->cd(1);
+  
+  sigTemplate->Scale(1./sigTemplate->Integral()); 
+  bkgTemplate->Scale(1./bkgTemplate->Integral());  
+  
+  double par[20] = {1., 1., 0.5, 0.3,
+		    bkgTemplate->GetMaximum(),-.3,-1., 0.01, 0.5, 0.01, 1., 1.};
+  int fit_status;
+
+  TF1 *f1 = new TF1("f1", exp_conv, -1., 11., 12);
+  f1->SetParameters(par);
+
+  c10->cd(1);
+  fit_status = sigTemplate->Fit(f1);
+  sigTemplate->Draw();
+  SigPDFnorm = f1->Integral(-1.,11.);
+  printf("status %d, sig area %3.3f \n", fit_status,f1->Integral(-1.,11.));
+  if ( fit_status > 0 ) {
+     printf("fit signal template failed. QUIT \n");
+     return;
+  }
+
+  Para.push_back(f1->GetParameter(0));
+  Para.push_back(f1->GetParameter(1));
+  Para.push_back(f1->GetParameter(2));
+  Para.push_back(f1->GetParameter(3)); 
+
+  c10->cd(2);
+  
+  TF1 *f3 = new TF1("f3", expinv_power, -1., 11., 12);
+  f3->SetParameters(f1->GetParameters());
+  f3->SetParLimits(5,-10.,1.);
+  f3->SetParLimits(6,-1.,2.);
+  f3->SetParLimits(7,0.,0.09);
+//   f3->SetParLimits(8,0.4,0.6);
+  
+  if(histoName.find("1.70")!= std::string::npos && 
+     histoName.find("50_80")!=std::string::npos)
+    {
+      cout << "find EE in pt bin 50--80" << endl;
+      f3->FixParameter(5,-0.1);
+    }
+  if(histoName.find("1.45")!=std::string::npos && 
+     histoName.find("80_120")!=std::string::npos)
+    {
+      cout << "find EB in pt bin 80--120" << endl;
+      f3->FixParameter(5,-0.1);
+    }
+
+//    f3->FixParameter(6,-0.05);
+  f3->FixParameter(8,0.5);
+  f3->FixParameter(0,f3->GetParameter(0));
+  f3->FixParameter(1,f3->GetParameter(1));
+  f3->FixParameter(2,f3->GetParameter(2));
+  f3->FixParameter(3,f3->GetParameter(3));
+
+  bkgTemplate->SetMaximum(bkgTemplate->GetMaximum()*3.);
+  fit_status = bkgTemplate->Fit(f3,"b");
+  bkgTemplate->Draw();
+  printf("status %d, bkg area %3.3f \n", fit_status,f3->Integral(-1.,11.)/hdata->GetBinWidth(2));
+  if ( fit_status > 0 ) {
+    printf("fit background template failed. QUIT \n");
+    return;
+  }
+
+  Para.push_back(f3->GetParameter(4));
+  Para.push_back(f3->GetParameter(5));
+  Para.push_back(f3->GetParameter(6));
+  Para.push_back(f3->GetParameter(7)); 
+  Para.push_back(f3->GetParameter(8)); 
+  BkgPDFnorm = f3->Integral(-1.,11);
+
+
+  //test PDFs
+  TCanvas *c11 = new TCanvas("c11","",1000,500);
+  c11->Divide(2,1);
+  c11->cd(1);
+  TF1 *f11 = new TF1("f11",exp_conv_norm, -1., 11., 12);
+  f11->SetParameters(f3->GetParameters());
+  f11->Draw();
+  printf(" SIG PDF area %2.3f \n", f11->Integral(-1.,11.));
+
+  c11->cd(2);
+  TF1 *f12 = new TF1("f12",expinv_power_norm, -1., 11., 12);
+  f12->SetParameters(f3->GetParameters());
+  f12->Draw();
+  printf(" BKG PDF area %2.3f \n", f12->Integral(-1.,11.));
+
+
+  c1->cd();
+  printf(" --------- before the fit ------------- \n");
+  printf("Nsig %2.3f, Nbg %2.3f, Ntemplate %3.3f \n", sigTemplate->Integral(), bkgTemplate->Integral(), ntemplate);
+  printf("Purity %2.3f, init size %4.3f,  fit sample size %4d\n", sigTemplate->Integral()/hsum->Integral(), hsum->Integral(), ndata);
+  printf(" -------------------------------------- \n");
+
+
+  printf( " -----  Got %d, %d, %d events for fit ----- \n ", dataColl.size(),
+	  sigColl.size(), bkgColl.size() );  
+//   if ( dataColl.size() != sigColl.size() || sigColl.size()!=bkgColl.size() ) {
+//     printf(" error ...  inconsistent hit collection size \n");
+//     return fitted;
+//   }
+
+  //========== Dump fit parameters
+  for(unsigned int ipara = 0; ipara < Para.size(); ipara++)
+    {
+      cout << "Parameter " << ipara << " = " << Para[ipara] << endl;
+
+    }
+
+
+  //--------------------------------------------------
+  //init parameters for fit
+  Double_t vstart[10] = {1., 1.};
+  vstart[0] = sigfrac*ndata;
+  vstart[1] = (1-sigfrac)*ndata;
+ 
+  TMinuit *gMinuit = new TMinuit(NPAR);  
+  gMinuit->Command("SET STR 1");
+  gMinuit->SetFCN(fcn);
+  Double_t arglist[10];
+  Int_t ierflg = 0;
+  
+  arglist[0] = 1;
+  gMinuit->mnexcm("SET ERR", arglist ,1,ierflg);
+  arglist[0] = 1;
+  gMinuit->mnexcm("SET PRINT", arglist ,1,ierflg);
+
+  Double_t step[] = { 0.1, 0.1,};
+
+  gMinuit->mnparm(0,  "Signal yield"  , vstart[0],  step[0], 0., ndata*2.  , ierflg);
+  gMinuit->mnparm(1,  "background yield"  , vstart[1],  step[1], 0., ndata*2. , ierflg);
+  
+
+  printf(" --------------------------------------------------------- \n");
+  printf(" Setting stragety = 2 \n ----------------------\n");
+  
+  arglist[0] = 2;
+  gMinuit->mnexcm("SET STRAT", arglist ,1,ierflg);
+
+  printf(" --------------------------------------------------------- \n");
+  printf(" Now ready for minimization step \n ----------------------\n");
+
+  arglist[0] = 2000; // number of iteration
+  arglist[1] = 1.;
+  gMinuit->mnexcm("MIGRAD", arglist ,2,ierflg);
+  printf (" -------------------------------------------- \n");
+  printf("Finished.  ierr = %2.2f \n", ierflg);
+
+  info.clear();
+  info_err.clear();
+
+  double para[NPAR+1],errpara[NPAR+1];
+  if ( ierflg == 0 ) 
+    {
+      for(int j=0; j<=NPAR-1;j++) {
+        gMinuit->GetParameter(j, para[j],errpara[j]);
+        para[NPAR] = dataColl.size();
+        info.push_back(para[j]);
+        info_err.push_back(errpara[j]);
+        printf("Parameter (yeild) %d = %f +- %f\n",j,para[j],errpara[j]);
+	
+      }
+      printf(" fitted yield %2.3f \n", (para[0]+para[1])/ndata );
+
+      info.push_back(sigColl.size());
+
+    }
+  else {
+    printf(" *********** Fit failed! ************\n");
+    gMinuit->GetParameter(0, para[0],errpara[0]);
+    gMinuit->GetParameter(1, para[1],errpara[1]);
+    para[0]=0.; errpara[0]=0.;
+  }
+
+  
+  // Print results
+  Double_t amin,edm,errdef;
+  Int_t nvpar,nparx,icstat;
+  gMinuit->mnstat(amin,edm,errdef,nvpar,nparx,icstat);
+  gMinuit->mnprin(1,amin);  
+  gMinuit->mnmatu(1);
+  printf(" ========= happy ending !? =========================== \n");
+  
+  printf("FCN =  %3.3f \n", amin);
+
+
+  // plot
+  c1->Draw();  
+  //gPad->SetLogy();
+   hdata->SetNdivisions(505,"XY");
+   hdata->SetXTitle("comb. ISO (GeV)");
+   hdata->SetYTitle("Entries");
+   hdata->SetTitle();
+   hdata->SetMarkerStyle(8);
+   hdata->SetMinimum(0.);
+   if ( hdata->GetMaximum()<15.) hdata->SetMaximum(20.);
+   else hdata->SetMaximum(hdata->GetMaximum()*1.2);
+   if(histoName.find("1.70")!= std::string::npos && 
+      histoName.find("15_20")!= std::string::npos)
+     hdata->SetMaximum(hdata->GetMaximum()*1.2);
+
+   hdata->Draw("p e");
+
+  f11->SetParameter(0, para[0]*f11->GetParameter(0)/f11->Integral(-1,11)*hdata->GetBinWidth(2));
+  f11->SetFillColor(5);
+  f11->SetLineColor(1);
+  //f11->SetFillColor(603);
+  f11->SetLineWidth(1);
+  f11->SetFillStyle(3001);
+  f11->Draw("same");
+
+  f12->SetParameter(4, para[1]*f12->GetParameter(4)/f12->Integral(-1,11)*hdata->GetBinWidth(2));
+  f12->SetFillColor(8);
+  f12->SetLineColor(1);
+  //f12->SetFillColor(603);
+  f12->SetLineWidth(1);
+  f12->SetFillStyle(3013);
+  f12->Draw("same");
+
+  TF1 *f13 = new TF1("f13",sum_norm, -1, 11,12);
+  f13->SetParameters(f12->GetParameters());
+  f13->SetParameter(0, para[0]*f11->GetParameter(0)/f11->Integral(-1,11)*hdata->GetBinWidth(2));
+  f13->SetParameter(4, para[1]*f12->GetParameter(4)/f12->Integral(-1,11)*hdata->GetBinWidth(2));  
+  f13->SetLineWidth(2);
+  f13->SetLineColor(1);
+  f13->Draw("same");
+  
+  printf("fit area %3.2f; sig area %3.2f; bg area %3.2f\n", f13->Integral(-1,11)/hdata->GetBinWidth(2),  f11->Integral(-1,11)/hdata->GetBinWidth(2),f12->Integral(-1,11)/hdata->GetBinWidth(2));
+
+   TLegend *tleg = new TLegend(0.5, 0.65, 0.95, 0.92);
+   char text[50];
+   tleg->SetHeader(dataInput->GetTitle());
+   tleg->SetFillColor(0);
+   tleg->SetShadowColor(0);
+   tleg->SetBorderSize(0);
+   sprintf(text,"Data %5.1f events",hdata->Integral());
+   tleg->AddEntry(hdata,text,"pl");
+   sprintf(text,"Fitted %5.1f events",f13->Integral(-1,11)/hdata->GetBinWidth(2));
+   tleg->AddEntry(f13,text,"l");
+   sprintf(text,"SIG %5.1f #pm %5.1f events",para[0], errpara[0]);
+   tleg->AddEntry(f11,text,"f");
+   sprintf(text,"BKG %5.1f #pm %5.1f events",para[1], errpara[1]);
+   tleg->AddEntry(f12,text,"f");
+   tleg->Draw();
+
+
+   gPad->RedrawAxis();
+
+  char fname[1000];
+  sprintf(fname,"plots/unbinned_Ifit_%s.pdf",hdata->GetName());
+  c1->SaveAs(fname);
+
+  printf("----- fit results with signal projection   ----------- \n");
+
+//   ftemplate->Close();
+
+
+  fitted[0] = para[0];
+  fitted[1] = errpara[0];
+  fitted[2] = para[1];
+  fitted[3] = errpara[1];
+  return fitted;
+}
+
+
+Double_t g(Double_t *v, Double_t *par)
+{
+  Double_t arg = 0;
+  arg = (v[0] - par[2]) / par[3];
+  
+  //Double_t area = par[1]/(par[3]*sqrt(2*3.1415926));
+  Double_t norm_area = 1/(par[3]*sqrt(2*3.1415926));
+  Double_t gaus = norm_area*TMath::Exp(-0.5*arg*arg);
+
+  if (gaus<=0) gaus=1e-10;
+  return gaus;
+}
+
+Double_t exp_conv (Double_t *v, Double_t *par)
+{
+  Double_t ctau = par[1];
+  //Double_t sigma = par[14];
+  Double_t sigma = par[3]; //using narrow prompt width
+  Double_t x = v[0]-par[2];
+
+  Double_t arg1 = TMath::Exp( 0.5*sigma*sigma/ctau/ctau - x/ctau );
+  Double_t arg2 = 1.0 - TMath::Freq( (sigma/ctau - x/sigma) );
+  //Double_t func = 1.0/ctau * arg1 * arg2;
+  Double_t func = par[0]/ctau * arg1 * arg2;
+
+  if (func<=0) func=1e-10;
+  return func;
+}
+
+Double_t exp_conv_norm(Double_t *v, Double_t *par)
+{
+  Double_t func = exp_conv(v,par) / SigPDFnorm;
+  if (func<=0) func=1e-10;
+  return func;
+}
+
+Double_t expinv_power(Double_t *v, Double_t *par){
+
+  Double_t x = v[0]-par[6];
+  Double_t func=0.;
+  if (x>0.) {
+    Double_t fitval = 1.- TMath::Exp(par[5]*x);
+    //func = par[4] * (1-par[9]*x) *TMath::Power(1-par[7]*x,par[8]) * fitval;
+    func = par[4] * TMath::Power(1-par[7]*x,par[8]) * fitval;
+    //func = par[4] * fitval;
+  }
+  return func;
+}
+
+Double_t expinv_power_norm (Double_t *v, Double_t *par)
+{
+  Double_t func = expinv_power(v,par)/BkgPDFnorm;
+  if (func<=0) func=1e-10;
+  return func;
+}
+
+Double_t sum_norm(Double_t *v, Double_t *par)
+{
+  Double_t func1 = exp_conv(v,par) / SigPDFnorm;
+  Double_t func2 = expinv_power(v,par)/BkgPDFnorm;
+  Double_t func = func1+func2;
+  if (func<=0) func=1e-10;
+  return func;
+}
