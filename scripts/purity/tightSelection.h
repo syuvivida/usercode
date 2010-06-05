fcd894108d9dc65277a131e470ea60a3141068b1
syu <>
2010-06-05 17:46:12 +0000

    add spike selections and modify EBsideband

--- scripts/purity/tightSelection.h
+++ scripts/purity/tightSelection.h
@@ -1,13 +1,17 @@
 #include <TCut.h>
 
-TCut removeSpikeCut = "((isEB && (seedSeverity!=3 && seedSeverity!=4 ) && (seedRecoFlag != 2) ) || isEE)";
+TCut removeSpikeCut = "((isEB && (seedSeverity!=3 && seedSeverity!=4 ) && (seedRecoFlag != 2) && sigmaIetaIeta > 0.002 ) || isEE)";
 
 TCut eventCut     = "( !TTBit[36] && !TTBit[37] && !TTBit[38] && !TTBit[39] && !vtxIsFake && vtxNdof > 4 && abs(vtxZ) <= 15) && HLT_Photon15_L1R";
 
 TCut rsCutEB = "pt > 15.0 && abs(eta)<1.45 && hadronicOverEm <  0.05 && sigmaIetaIeta < 0.01 && (ecalRecHitSumEtConeDR04+hcalTowerSumEtConeDR04+trkSumPtHollowConeDR04) < 11.0";
 TCut rsCutEE = "pt > 15.0 && abs(eta) > 1.7 && abs(eta) < 2.5 && hadronicOverEm <  0.05 && abs(ESRatio) > 0.1 && (ecalRecHitSumEtConeDR04+hcalTowerSumEtConeDR04+trkSumPtHollowConeDR04) < 11.0";
 
-TCut rsCutSidebandEB = "pt > 15.0 && abs(eta)<1.45 && hadronicOverEm <  0.05 && sigmaIetaIeta > 0.012 &&sigmaIetaIeta<0.015 && (ecalRecHitSumEtConeDR04+hcalTowerSumEtConeDR04+trkSumPtHollowConeDR04) < 11.0";
+
+TCut spikeCut = !removeSpikeCut + "isEB";
+
+
+TCut rsCutSidebandEB = "pt > 15.0 && abs(eta)<1.45 && hadronicOverEm <  0.05 && sigmaIetaIeta > 0.011 &&sigmaIetaIeta<0.012 && (ecalRecHitSumEtConeDR04+hcalTowerSumEtConeDR04+trkSumPtHollowConeDR04) < 11.0";
 
 TCut rsCutSidebandEE = "pt > 15.0 && abs(eta) > 1.7 && abs(eta) < 2.5 && hadronicOverEm <  0.05 && abs(ESRatio) < 0.1 && abs(ESRatio)>0.05 && (ecalRecHitSumEtConeDR04+hcalTowerSumEtConeDR04+trkSumPtHollowConeDR04) < 11.0";
 
