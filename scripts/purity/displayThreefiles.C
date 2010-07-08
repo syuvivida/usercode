f2e9e8b60be13e50b2a220dd6cf5d0c78ae8560f
syu <>
2010-07-08 12:25:24 +0000

    add KS and chi2 test

--- scripts/purity/displayThreefiles.C
+++ scripts/purity/displayThreefiles.C
@@ -7,11 +7,12 @@ void displayThreefiles()
   setTDRStyle();
   gStyle->SetOptStat(0);
 
-  std::string var1 = "h_EB_comb3IsoSB_EGdata_pt15";
-  std::string var2 = "h_EB_comb3IsoSB_EGdata_pt15";
-  std::string var3 = "h_EB_comb3Iso_bkg_pt15";
+  std::string var1 = "h_EB_comb3IsoSB_EGdata_pt20";
+  std::string var2 = "h_EB_comb3IsoSB_EGdata_pt20";
+  std::string var3 = "h_EB_comb3Iso_bkg_pt20";
 
-  std::string xtitle = "Iso_{ECAL}+Iso_{HCAL}+Iso_{TRK} (GeV)";
+//   std::string xtitle = "Iso_{ECAL}+Iso_{HCAL}+Iso_{TRK} (GeV)";
+  std::string xtitle = "Iso";
   std::string ytitle = "A.U.";
 
   TH1F* h1;
@@ -22,9 +23,9 @@ void displayThreefiles()
 
   TCanvas* c1 = new TCanvas("c1","",500,500);
 
-  TFile *f1 = TFile::Open("SBDataTemplate_proj_comb.root");
-  TFile *f2 = TFile::Open("SBMCTemplate_proj_comb.root");
-  TFile *f3 = TFile::Open("SBMCTemplate_proj_comb.root");
+  TFile *f1 = TFile::Open("Integral_SBDataTemplate_131511_139239_362_PMMC_proj_comb.root");
+  TFile *f2 = TFile::Open("Intergral_SBMCTemplate_131511_139239_362_PMMC_proj_comb.root");
+  TFile *f3 = TFile::Open("Intergral_SBMCTemplate_131511_139239_362_PMMC_proj_comb.root");
 
   h1  = (TH1F*)(f1->Get(var1.data()));
   h1->SetXTitle(xtitle.data());  
@@ -41,6 +42,10 @@ void displayThreefiles()
   h3->Rebin(12);
   h3->SetYTitle(ytitle.data());  
 
+  h1->GetYaxis()->SetNdivisions(5);
+  h2->GetYaxis()->SetNdivisions(5);
+  h3->GetYaxis()->SetNdivisions(5);
+
   h1->GetYaxis()->SetDecimals();
   h2->GetYaxis()->SetDecimals();
   h3->GetYaxis()->SetDecimals();
@@ -55,51 +60,84 @@ void displayThreefiles()
   h2->SetMarkerColor(2);
   h2->SetMarkerSize(1);
   h2->SetMarkerStyle(23);
+  h2->SetLineWidth(3);
 
   h3->SetLineColor(kGreen-3);
   h3->SetMarkerColor(kGreen-3);
   h3->SetMarkerSize(1);
   h3->SetMarkerStyle(26);
-
+  h3->SetLineStyle(2);
+  h3->SetLineWidth(3);
 
 
   float scale1 = 1.0/(float)h1->Integral();
   float scale2 = 1.0/(float)h2->Integral();
   float scale3 = 1.0/(float)h3->Integral();
 
-   h1->SetTitle("");
-   h2->SetTitle("");
-   h1->Sumw2();
-   h1->Scale(scale1);
-   h2->Sumw2();
-   h2->Scale(scale2);
-   h3->Sumw2();
-   h3->Scale(scale3);
-
-   float max1   = h1->GetBinError(h1->GetMaximumBin()) + h1->GetMaximum();
-   float max2   = h2->GetBinError(h2->GetMaximumBin()) + h2->GetMaximum();
-
-
-   h1->Draw("histe");
-   h2->Draw("histe,same");
-   h3->Draw("histe,same");
+  h1->SetTitle("");
+  h2->SetTitle("");
+  h1->Sumw2();
+  h1->Scale(scale1);
+  h2->Sumw2();
+  h2->Scale(scale2);
+  h3->Sumw2();
+  h3->Scale(scale3);
+  
+  float max1   = h1->GetBinError(h1->GetMaximumBin()) + h1->GetMaximum();
+  float max2   = h2->GetBinError(h2->GetMaximumBin()) + h2->GetMaximum();
 
 
-   TLegend* leg = new TLegend(0.40,0.27,0.60,0.42);
-   leg->SetFillColor(0);
-   leg->SetFillStyle(0);
-   leg->SetTextSize(0.04);
-   leg->SetBorderSize(0);
-   leg->AddEntry(h1,"Data sideband");
-   leg->AddEntry(h2,"MC sideband");   
-   leg->AddEntry(h3,"MC background truth");   
-   leg->Draw("same");
+//   h2->Draw("histe");
+//   h1->Draw("histe,same");
+//   h3->Draw("histe,same");
 
-   std::string filename;
-   std::string psname = "Iso_datadriven_background";
-   filename = psname + ".eps";
-   c1->Print(filename.data());
-   filename = psname + ".gif";
-   c1->Print(filename.data());
+  h1->Draw("histe");
+  h2->Draw("hist,same");
+  h3->Draw("hist,same");
+
+
+  TLegend* leg = new TLegend(0.40,0.27,0.60,0.42);
+  //    leg->SetHeader("p_{T}[50,80]");
+  leg->SetFillColor(0);
+  leg->SetFillStyle(0);
+  leg->SetTextSize(0.04);
+  leg->SetBorderSize(0);
+  leg->AddEntry(h1,"Data sideband");
+  leg->AddEntry(h2,"MC sideband","l");   
+  leg->AddEntry(h3,"MC background truth","l");   
+  leg->Draw("same");
+
+  std::string filename;
+  std::string psname = "Iso_datadriven_background";
+  filename = psname + ".eps";
+  c1->Print(filename.data());
+  filename = psname + ".gif";
+  c1->Print(filename.data());
+
+  double chi2;
+  int nbins;
+
+  chi2NbinsCompare(h1,h2,chi2,nbins);
+
+  cout << "chi2 test for sideband data and sideband MC = " << 
+    TMath::Prob(chi2,nbins) << endl;
+  cout << "KS test for sideband data and sideband MC = " << 
+    h2->KolmogorovTest(h1,"X") << endl;
+  cout << "KS test 2 for sideband data and sideband MC = " << 
+    h2->KolmogorovTest(h1) << endl;
+
+
+  chi2NbinsCompare(h2,h3,chi2,nbins);
+
+  cout << "chi2 test for sideband MC and background MC = " << 
+    TMath::Prob(chi2,nbins) << endl;
+
+  cout << "KS test for sideband MC and background MC = " << 
+    h2->KolmogorovTest(h3,"X") << endl;
+
+  cout << "KS test2 for sideband MC and background MC = " << 
+    h2->KolmogorovTest(h3) << endl;
+
+
 }
 		     
