a9ce584613fdfb2ec1d392300179a5516e369388
syu <>
2012-02-10 14:00:35 +0000

    macros to call classes

---
+++ scripts/gjet/runLoop.C
@@ -0,0 +1,17 @@
+
+void runLoop(std::string className, std::string fileName)
+{
+  TStopwatch* myWatch = new TStopwatch();
+  myWatch->Start();
+  std::string command = ".L " + className + ".C++";
+  gROOT->ProcessLine(command.data());
+
+  command = className + " a(\"" + fileName + "\");";
+  gROOT->ProcessLine(command.data());
+
+  command = "a.Loop()";
+  gROOT->ProcessLine(command.data());
+  myWatch->Stop();
+  cout << myWatch->RealTime() << " seconds has passed! " << endl; 
+
+}
