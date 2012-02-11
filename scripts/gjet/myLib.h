0e348372c633647481d0417bf8ab2bd8f4ebc0ac
syu <>
2012-02-11 21:07:34 +0000

    add yCOM variable

--- scripts/gjet/myLib.h
+++ scripts/gjet/myLib.h
@@ -3,73 +3,79 @@
 
 namespace eiko {
 
-Bool_t separated(TLorentzVector l1, TLorentzVector l2);
-Double_t zgamma(TLorentzVector l1, TLorentzVector l2);
-Double_t deltaPhi(TLorentzVector l1, TLorentzVector l2);
-Double_t cosThetaStar(TLorentzVector l1, TLorentzVector l2);
-Double_t chiPair(TLorentzVector l1, TLorentzVector l2);
-Double_t pstar(TLorentzVector l1, TLorentzVector l2);
-Double_t yB(TLorentzVector l1, TLorentzVector l2);
+  Bool_t separated(TLorentzVector l1, TLorentzVector l2);
+  Double_t zgamma(TLorentzVector l1, TLorentzVector l2);
+  Double_t deltaPhi(TLorentzVector l1, TLorentzVector l2);
+  Double_t cosThetaStar(TLorentzVector l1, TLorentzVector l2);
+  Double_t chiPair(TLorentzVector l1, TLorentzVector l2);
+  Double_t pstar(TLorentzVector l1, TLorentzVector l2);
+  Double_t yB(TLorentzVector l1, TLorentzVector l2);
+  Double_t yCOM(TLorentzVector l1, TLorentzVector l2);
+
+  Bool_t separated(TLorentzVector l1, TLorentzVector l2) { 
+    Bool_t result = false;
+    if(l1.DeltaR(l2)>0.5)result=true; 
+    return result;
+  }
+
+  // variable proposed by JETPHOX authors
+  Double_t zgamma(TLorentzVector l1, TLorentzVector l2) { 
+    Double_t dphi = l1.Phi()-l2.Phi();
+    while (dphi > TMath::Pi()) dphi -= 2*TMath::Pi();
+    while (dphi <= -TMath::Pi()) dphi += 2*TMath::Pi();
+
+    Double_t result = fabs(l2.Pt())>1e-6? - (l1.Pt()/l2.Pt())*TMath::Cos(dphi) : -999.0;
+
+    return result;
+  }
+
+  Double_t deltaPhi(TLorentzVector l1, TLorentzVector l2) { 
+    Double_t result = l1.Phi()-l2.Phi();
+    while (result > TMath::Pi()) result -= 2*TMath::Pi();
+    while (result <= -TMath::Pi()) result += 2*TMath::Pi();
+    return fabs(result);
+  }
+
+  Double_t cosThetaStar(TLorentzVector l1, TLorentzVector l2) { 
+
+    Double_t ystar = 0.5*fabs(l1.Rapidity()-l2.Rapidity());
+    Double_t result = TMath::TanH(ystar);
+    return result;
+
+  }
+
+  Double_t chiPair(TLorentzVector l1, TLorentzVector l2) { 
+
+    Double_t ystar = 0.5*fabs(l1.Rapidity()-l2.Rapidity());
+    Double_t result = TMath::Exp(2*ystar);
+    return result;
+  }
 
 
-Bool_t separated(TLorentzVector l1, TLorentzVector l2) { 
-  Bool_t result = false;
-  if(l1.DeltaR(l2)>0.5)result=true; 
-  return result;
-}
+  // some CM frame variables, pstar is momentum magnitude of photon in the 
+  // center of mass frame pt/sinTheta*
 
-// variable proposed by JETPHOX authors
-Double_t zgamma(TLorentzVector l1, TLorentzVector l2) { 
-  Double_t dphi = l1.Phi()-l2.Phi();
-  while (dphi > TMath::Pi()) dphi -= 2*TMath::Pi();
-  while (dphi <= -TMath::Pi()) dphi += 2*TMath::Pi();
+  Double_t pstar(TLorentzVector l1, TLorentzVector l2) { 
 
-  Double_t result = fabs(l2.Pt())>1e-6? - (l1.Pt()/l2.Pt())*TMath::Cos(dphi) : -999.0;
+    Double_t ystar = 0.5*fabs(l1.Rapidity()-l2.Rapidity());  
+    Double_t result = l1.Pt()*TMath::CosH(ystar);
+    return result;
 
-  return result;
-}
-
-Double_t deltaPhi(TLorentzVector l1, TLorentzVector l2) { 
-  Double_t result = l1.Phi()-l2.Phi();
-  while (result > TMath::Pi()) result -= 2*TMath::Pi();
-  while (result <= -TMath::Pi()) result += 2*TMath::Pi();
-  return fabs(result);
-}
-
-Double_t cosThetaStar(TLorentzVector l1, TLorentzVector l2) { 
-
-  Double_t ystar = 0.5*fabs(l1.Rapidity()-l2.Rapidity());
-  Double_t result = TMath::TanH(ystar);
-  return result;
-
-}
-
-Double_t chiPair(TLorentzVector l1, TLorentzVector l2) { 
-
-  Double_t ystar = 0.5*fabs(l1.Rapidity()-l2.Rapidity());
-  Double_t result = TMath::Exp(2*ystar);
-  return result;
-}
+  }
 
 
-// some CM frame variables, pstar is momentum magnitude of photon in the 
-// center of mass frame pt/sinTheta*
+  Double_t yB(TLorentzVector l1, TLorentzVector l2) { 
 
-Double_t pstar(TLorentzVector l1, TLorentzVector l2) { 
+    Double_t result = 0.5*(l1.Rapidity()+l2.Rapidity());  
+    return result;
 
-  Double_t ystar = 0.5*fabs(l1.Rapidity()-l2.Rapidity());  
-  Double_t result = l1.Pt()*TMath::CosH(ystar);
-  return result;
+  }
 
-}
+  Double_t yCOM(TLorentzVector l1, TLorentzVector l2){
 
+    Double_t result =0.5*(l1.Rapidity()-l2.Rapidity());
+    return result;
 
-Double_t yB(TLorentzVector l1, TLorentzVector l2) { 
-
-  Double_t result = 0.5*(l1.Rapidity()+l2.Rapidity());  
-  return result;
-
-}
-
+  }
 
 }
