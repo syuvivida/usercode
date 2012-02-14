00df0fe93efa86e699734262f0bcaa74e1da3257
syu <>
2012-02-14 20:06:52 +0000

    make the application of pileup correction default

--- scripts/gjet/yj_angularmc_eff.h
+++ scripts/gjet/yj_angularmc_eff.h
@@ -314,7 +314,7 @@ public :
    virtual Int_t    GetEntry(Long64_t entry);
    virtual Long64_t LoadTree(Long64_t entry);
    virtual void     Init(TTree *tree);
-   virtual void     Loop(bool applyCOMCut=false, bool applyPileupCorr=false);
+   virtual void     Loop(bool applyCOMCut=false, bool applyPileupCorr=true);
    virtual Bool_t   Notify();
    virtual void     Show(Long64_t entry = -1);
    virtual Int_t    phoDecCode(Long64_t entry, Int_t ipho);
