dc0f0d27ae748ec97c100f435835ba0ab972d298
syu <>
2012-04-03 19:10:45 +0000

    study generator-level rapidity difference and sum distribution with basic muon and electron cuts

---
+++ scripts/zjet/gen_distribution.C
@@ -0,0 +1,195 @@
+#define gen_distribution_cxx
+#include "gen_distribution.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TLorentzVector.h>
+
+
+void gen_distribution::Loop(int lepID, int DEBUG)
+{
+   if (fChain == 0) return;
+   TH1D* h_y_template = new TH1D("h_y_template","",25,-2.5,2.5);
+
+   TH1D* h_yB    = (TH1D*)h_y_template->Clone("h_yB");
+   h_yB->SetXTitle("0.5(y_{Z}+y_{jet})");
+
+   TH1D* h_ystar = (TH1D*)h_y_template->Clone("h_ystar");
+   h_ystar->SetXTitle("0.5(y_{Z}-y_{jet})");
+
+
+
+   Long64_t nentries = fChain->GetEntriesFast();
+
+   cout << _inputFileName << " has " << nentries << " entries." << endl;
+   std::string leptonName;
+   if(abs(lepID)==13)leptonName = "muon";
+   else if(abs(lepID)==11)leptonName = "electron";
+
+   cout << "studying " << leptonName << endl;
+
+   // dummy proof, in case someone put a negative number
+   int leptonPID = abs(lepID); 
+   
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+//       if(jentry > 50000)break;
+
+      int lepPlusIndex = -1;
+      int lepMinusIndex = -1;
+
+
+      for(unsigned int igen=0; igen < genParId_->size(); igen++){
+
+	if(lepPlusIndex < 0 && fabs(genParId_->at(igen)-(-leptonPID))<1e-6 && 
+	   fabs(genParSt_->at(igen)-1) < 1e-6)
+	  lepPlusIndex = igen;
+
+	if(lepMinusIndex < 0 && fabs(genParId_->at(igen)-(leptonPID))<1e-6 && 
+	   fabs(genParSt_->at(igen)-1) < 1e-6)
+	  lepMinusIndex = igen;
+
+
+	// need to add a line and see if this is a daughter of Z-> tau tau
+	if(lepPlusIndex >= 0 && lepMinusIndex >=0)
+	  break;
+	
+      }
+
+      // do not find m+ or lep-
+      if(lepPlusIndex < 0 || lepMinusIndex < 0)continue;
+
+      if(DEBUG==1){
+	cout << "lepPlusIndex = " << lepPlusIndex << "\t pt=" << 
+	  genParPt_->at(lepPlusIndex) << "\t eta=" << 
+	  genParEta_->at(lepPlusIndex) << endl;
+	cout << "lepMinusIndex = " << lepMinusIndex << "\t pt=" << 
+	  genParPt_->at(lepMinusIndex) << "\t eta=" << 
+	  genParEta_->at(lepMinusIndex) << endl;
+      }
+
+      int nPt20=0;
+      int nPt10=0;
+
+      int indexNumbers[2] = {lepPlusIndex, lepMinusIndex};
+
+      for(int ip=0; ip < 2; ip++){
+
+	double pt  = genParPt_->at(indexNumbers[ip]);
+	double eta = genParEta_->at(indexNumbers[ip]);
+
+	if(leptonPID==13 && pt > 20.0 && fabs(eta) < 2.1)nPt20++;
+	if(leptonPID==13 && pt > 10.0 && fabs(eta) < 2.1)nPt10++;
+
+	if(leptonPID==11 && pt > 20.0 && fabs(eta) > 1.566 && 
+	   fabs(eta) < 2.5)nPt20++;
+
+	if(leptonPID==11 && pt > 20.0 && fabs(eta) > 0.0 && 
+	   fabs(eta) < 1.446)nPt20++;
+
+      }
+
+      if(DEBUG==1)
+	cout << "nPt20 = " << nPt20 << "\t nPt10 = " << nPt10 << endl;
+
+      if(leptonPID==13 && (nPt20 < 1 || nPt10 < 2))continue;
+      if(leptonPID==11 && (nPt20 < 2))continue;
+
+
+      TLorentzVector l4_lepp(0,0,0,0);
+
+      l4_lepp.SetPtEtaPhiE(genParPt_->at(lepPlusIndex),
+			   genParEta_->at(lepPlusIndex),
+			   genParPhi_->at(lepPlusIndex),
+			   genParE_->at(lepPlusIndex));
+
+      TLorentzVector l4_lepm(0,0,0,0);
+
+      l4_lepm.SetPtEtaPhiE(genParPt_->at(lepMinusIndex),
+			   genParEta_->at(lepMinusIndex),
+			   genParPhi_->at(lepMinusIndex),
+			   genParE_->at(lepMinusIndex));
+
+
+      TLorentzVector l4_z = l4_lepp + l4_lepm;
+
+
+      double mll = l4_z.M();
+
+      if(leptonPID==13 && (mll < 76 || mll > 106))continue;
+      if(leptonPID==11 && (mll < 70 || mll > 110))continue;
+
+      if(DEBUG==1)
+	cout << "dilepton mass = " << mll << endl;
+      
+
+      // now look for jets
+      double maxGenJetPt = -9999;
+      int maxGenJetIndex = -1;
+
+
+      for(unsigned int ij = 0; ij < genJetPt_->size(); ij ++){
+
+	double thisGenJetPt  = genJetPt_->at(ij);
+	double thisGenJetEta = genJetEta_->at(ij);
+
+	if(thisGenJetPt < 30.0)continue;
+	if(fabs(thisGenJetEta) > 2.4)continue;
+
+	if(thisGenJetPt > maxGenJetPt)
+	  {
+	    maxGenJetPt = thisGenJetPt;
+	    maxGenJetIndex = ij;
+	  }
+
+
+      }
+      
+      if(DEBUG==1)
+	cout << "max gen jet index = " << maxGenJetIndex << endl;
+
+      if(maxGenJetIndex < 0)continue;
+
+      
+      TLorentzVector l4_j(0,0,0,0);
+      l4_j.SetPtEtaPhiE(genJetPt_->at(maxGenJetIndex),
+			genJetEta_->at(maxGenJetIndex),
+			genJetPhi_->at(maxGenJetIndex),
+			genJetE_->at(maxGenJetIndex));
+
+
+      double yz = l4_z.Rapidity();
+      double yj = l4_j.Rapidity();
+
+      double yB = 0.5*(yz + yj);
+      double ystar = 0.5*(yz-yj);
+
+      h_yB->Fill(yB);
+      h_ystar->Fill(ystar);
+
+
+   } // end of loop over entries
+
+   std::string prefix = "genHisto_";
+   std::string remword  ="/scratch/syu/test_sherpa/genonly/";
+
+   size_t pos  = _inputFileName.find(remword);
+
+   if(pos!= std::string::npos)
+     _inputFileName.swap(_inputFileName.erase(pos,remword.length()));
+
+
+   TFile* outFile = new TFile(Form("genHisto_%s_%s",leptonName.data(),
+				   _inputFileName.data()),"recreate");       
+        
+ 
+   h_yB->Write();
+   h_ystar->Write();
+
+   outFile->Close();
+
+}
---
+++ scripts/zjet/gen_distribution.h
@@ -0,0 +1,298 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Tue Apr  3 17:56:46 2012 by ROOT version 5.27/06b
+// from TTree tree/tree
+// found on file: /scratch/syu/test_sherpa/genonly/zjets_mc_1_1_WVP.root
+//////////////////////////////////////////////////////////
+
+#ifndef gen_distribution_h
+#define gen_distribution_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+   const Int_t kMaxEvtInfo_VertexX = 1;
+   const Int_t kMaxEvtInfo_VertexY = 1;
+   const Int_t kMaxEvtInfo_VertexZ = 1;
+   const Int_t kMaxptHat = 1;
+   const Int_t kMaxmcWeight = 1;
+   const Int_t kMaxgenParPx = 1;
+   const Int_t kMaxgenParPy = 1;
+   const Int_t kMaxgenParPz = 1;
+   const Int_t kMaxgenParE = 1;
+   const Int_t kMaxgenParP = 1;
+   const Int_t kMaxgenParTheta = 1;
+   const Int_t kMaxgenParPt = 1;
+   const Int_t kMaxgenParEta = 1;
+   const Int_t kMaxgenParPhi = 1;
+   const Int_t kMaxgenParEt = 1;
+   const Int_t kMaxgenParQ = 1;
+   const Int_t kMaxgenParId = 1;
+   const Int_t kMaxgenParSt = 1;
+   const Int_t kMaxgenJetPx = 1;
+   const Int_t kMaxgenJetPy = 1;
+   const Int_t kMaxgenJetPz = 1;
+   const Int_t kMaxgenJetE = 1;
+   const Int_t kMaxgenJetP = 1;
+   const Int_t kMaxgenJetTheta = 1;
+   const Int_t kMaxgenJetPt = 1;
+   const Int_t kMaxgenJetEta = 1;
+   const Int_t kMaxgenJetPhi = 1;
+   const Int_t kMaxgenJetEt = 1;
+   const Int_t kMaxgenJetQ = 1;
+
+class gen_distribution {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+   // Declaration of leaf types
+   Double_t        PU_weight;
+   Int_t           EvtInfo_EventNum;
+   Int_t           EvtInfo_RunNum;
+   Int_t           EvtInfo_LumiSection;
+   Int_t           EvtInfo_BunchXing;
+   Int_t           EvtInfo_NumVtx;
+   vector<double>  *EvtInfo_VertexX_;
+   vector<double>  *EvtInfo_VertexY_;
+   vector<double>  *EvtInfo_VertexZ_;
+   Double_t        ptHat_;
+   Double_t        mcWeight_;
+   vector<double>  *genParPx_;
+   vector<double>  *genParPy_;
+   vector<double>  *genParPz_;
+   vector<double>  *genParE_;
+   vector<double>  *genParP_;
+   vector<double>  *genParTheta_;
+   vector<double>  *genParPt_;
+   vector<double>  *genParEta_;
+   vector<double>  *genParPhi_;
+   vector<double>  *genParEt_;
+   vector<double>  *genParQ_;
+   vector<double>  *genParId_;
+   vector<double>  *genParSt_;
+   vector<double>  *genJetPx_;
+   vector<double>  *genJetPy_;
+   vector<double>  *genJetPz_;
+   vector<double>  *genJetE_;
+   vector<double>  *genJetP_;
+   vector<double>  *genJetTheta_;
+   vector<double>  *genJetPt_;
+   vector<double>  *genJetEta_;
+   vector<double>  *genJetPhi_;
+   vector<double>  *genJetEt_;
+   vector<double>  *genJetQ_;
+
+   // List of branches
+   TBranch        *b_PU_weight;   //!
+   TBranch        *b_EvtInfo_EventNum;   //!
+   TBranch        *b_EvtInfo_RunNum;   //!
+   TBranch        *b_EvtInfo_LumiSection;   //!
+   TBranch        *b_EvtInfo_BunchXing;   //!
+   TBranch        *b_EvtInfo_NumVtx;   //!
+   TBranch        *b_EvtInfo_VertexX_;   //!
+   TBranch        *b_EvtInfo_VertexY_;   //!
+   TBranch        *b_EvtInfo_VertexZ_;   //!
+   TBranch        *b_ptHat_;   //!
+   TBranch        *b_mcWeight_;   //!
+   TBranch        *b_genParPx_;   //!
+   TBranch        *b_genParPy_;   //!
+   TBranch        *b_genParPz_;   //!
+   TBranch        *b_genParE_;   //!
+   TBranch        *b_genParP_;   //!
+   TBranch        *b_genParTheta_;   //!
+   TBranch        *b_genParPt_;   //!
+   TBranch        *b_genParEta_;   //!
+   TBranch        *b_genParPhi_;   //!
+   TBranch        *b_genParEt_;   //!
+   TBranch        *b_genParQ_;   //!
+   TBranch        *b_genParId_;   //!
+   TBranch        *b_genParSt_;   //!
+   TBranch        *b_genJetPx_;   //!
+   TBranch        *b_genJetPy_;   //!
+   TBranch        *b_genJetPz_;   //!
+   TBranch        *b_genJetE_;   //!
+   TBranch        *b_genJetP_;   //!
+   TBranch        *b_genJetTheta_;   //!
+   TBranch        *b_genJetPt_;   //!
+   TBranch        *b_genJetEta_;   //!
+   TBranch        *b_genJetPhi_;   //!
+   TBranch        *b_genJetEt_;   //!
+   TBranch        *b_genJetQ_;   //!
+
+   gen_distribution(std::string filename,TTree *tree=0);
+   virtual ~gen_distribution();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop(int lepID, int DEBUG=1); // 13: muon, 11: electron
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+
+   std::string _inputFileName;
+
+
+};
+
+#endif
+
+#ifdef gen_distribution_cxx
+gen_distribution::gen_distribution(std::string filename, TTree *tree)
+{
+  if (tree == 0) {
+    TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject(filename.data());
+    if (!f) {
+      f = new TFile(filename.data());
+      f->cd(Form("%s:/tree",filename.data()));
+    }
+    tree = (TTree*)gDirectory->Get("tree");
+
+  }
+  Init(tree);
+  _inputFileName = filename;
+
+
+}
+
+gen_distribution::~gen_distribution()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t gen_distribution::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t gen_distribution::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (!fChain->InheritsFrom(TChain::Class()))  return centry;
+   TChain *chain = (TChain*)fChain;
+   if (chain->GetTreeNumber() != fCurrent) {
+      fCurrent = chain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void gen_distribution::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set object pointer
+   EvtInfo_VertexX_ = 0;
+   EvtInfo_VertexY_ = 0;
+   EvtInfo_VertexZ_ = 0;
+   genParPx_ = 0;
+   genParPy_ = 0;
+   genParPz_ = 0;
+   genParE_ = 0;
+   genParP_ = 0;
+   genParTheta_ = 0;
+   genParPt_ = 0;
+   genParEta_ = 0;
+   genParPhi_ = 0;
+   genParEt_ = 0;
+   genParQ_ = 0;
+   genParId_ = 0;
+   genParSt_ = 0;
+   genJetPx_ = 0;
+   genJetPy_ = 0;
+   genJetPz_ = 0;
+   genJetE_ = 0;
+   genJetP_ = 0;
+   genJetTheta_ = 0;
+   genJetPt_ = 0;
+   genJetEta_ = 0;
+   genJetPhi_ = 0;
+   genJetEt_ = 0;
+   genJetQ_ = 0;
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("PU_weight", &PU_weight, &b_PU_weight);
+   fChain->SetBranchAddress("EvtInfo_EventNum", &EvtInfo_EventNum, &b_EvtInfo_EventNum);
+   fChain->SetBranchAddress("EvtInfo_RunNum", &EvtInfo_RunNum, &b_EvtInfo_RunNum);
+   fChain->SetBranchAddress("EvtInfo_LumiSection", &EvtInfo_LumiSection, &b_EvtInfo_LumiSection);
+   fChain->SetBranchAddress("EvtInfo_BunchXing", &EvtInfo_BunchXing, &b_EvtInfo_BunchXing);
+   fChain->SetBranchAddress("EvtInfo_NumVtx", &EvtInfo_NumVtx, &b_EvtInfo_NumVtx);
+   fChain->SetBranchAddress("EvtInfo_VertexX_", &EvtInfo_VertexX_, &b_EvtInfo_VertexX_);
+   fChain->SetBranchAddress("EvtInfo_VertexY_", &EvtInfo_VertexY_, &b_EvtInfo_VertexY_);
+   fChain->SetBranchAddress("EvtInfo_VertexZ_", &EvtInfo_VertexZ_, &b_EvtInfo_VertexZ_);
+   fChain->SetBranchAddress("ptHat_", &ptHat_, &b_ptHat_);
+   fChain->SetBranchAddress("mcWeight_", &mcWeight_, &b_mcWeight_);
+   fChain->SetBranchAddress("genParPx_", &genParPx_, &b_genParPx_);
+   fChain->SetBranchAddress("genParPy_", &genParPy_, &b_genParPy_);
+   fChain->SetBranchAddress("genParPz_", &genParPz_, &b_genParPz_);
+   fChain->SetBranchAddress("genParE_", &genParE_, &b_genParE_);
+   fChain->SetBranchAddress("genParP_", &genParP_, &b_genParP_);
+   fChain->SetBranchAddress("genParTheta_", &genParTheta_, &b_genParTheta_);
+   fChain->SetBranchAddress("genParPt_", &genParPt_, &b_genParPt_);
+   fChain->SetBranchAddress("genParEta_", &genParEta_, &b_genParEta_);
+   fChain->SetBranchAddress("genParPhi_", &genParPhi_, &b_genParPhi_);
+   fChain->SetBranchAddress("genParEt_", &genParEt_, &b_genParEt_);
+   fChain->SetBranchAddress("genParQ_", &genParQ_, &b_genParQ_);
+   fChain->SetBranchAddress("genParId_", &genParId_, &b_genParId_);
+   fChain->SetBranchAddress("genParSt_", &genParSt_, &b_genParSt_);
+   fChain->SetBranchAddress("genJetPx_", &genJetPx_, &b_genJetPx_);
+   fChain->SetBranchAddress("genJetPy_", &genJetPy_, &b_genJetPy_);
+   fChain->SetBranchAddress("genJetPz_", &genJetPz_, &b_genJetPz_);
+   fChain->SetBranchAddress("genJetE_", &genJetE_, &b_genJetE_);
+   fChain->SetBranchAddress("genJetP_", &genJetP_, &b_genJetP_);
+   fChain->SetBranchAddress("genJetTheta_", &genJetTheta_, &b_genJetTheta_);
+   fChain->SetBranchAddress("genJetPt_", &genJetPt_, &b_genJetPt_);
+   fChain->SetBranchAddress("genJetEta_", &genJetEta_, &b_genJetEta_);
+   fChain->SetBranchAddress("genJetPhi_", &genJetPhi_, &b_genJetPhi_);
+   fChain->SetBranchAddress("genJetEt_", &genJetEt_, &b_genJetEt_);
+   fChain->SetBranchAddress("genJetQ_", &genJetQ_, &b_genJetQ_);
+   Notify();
+}
+
+Bool_t gen_distribution::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void gen_distribution::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t gen_distribution::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef gen_distribution_cxx
