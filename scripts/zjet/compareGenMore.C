ecf26ea518c8f9fb85e32002916be06b11e6e291
syu <>
2012-05-04 18:32:57 +0000

    use arrays

--- scripts/zjet/compareGenMore.C
+++ scripts/zjet/compareGenMore.C
@@ -1,87 +1,80 @@
 #include "/afs/cern.ch/user/s/syu/scripts/setTDRStyle.C"
 
-void compareGenMore(std::string mcfile1, std::string mcfile2, std::string mcfile3,
-		    std::string var1, std::string var2="", std::string var3="",
-		    std::string output="test",
-		    std::string headertitle="Z(#rightarrow ee)+1 jet",
-		    std::string mcName1="Sherpa",
-		    std::string mcName2="Madgraph",
-		    std::string mcName3="MCFM",
-		    float xmin=-9999.0, float xmax=-9999.0,
-		    bool logScale=false)
+void compareGenMore(
+		 std::string mcfile1, std::string mcfile2, std::string mcfile3,
+		 std::string output="test",
+		 std::string var1="h_ystar", std::string var2="h_ystar", std::string var3="id6",
+		 float xmin=-9999.0, float xmax=-9999.0,
+		 std::string headertitle="Z(#rightarrow ee)+1 jet",
+		 std::string mcName1="Sherpa",
+		 std::string mcName2="Madgraph",
+		 std::string mcName3="MCFM",
+		 bool logScale=false)
 {
   
   setTDRStyle();
   gStyle->SetOptStat(0);
 
-  TH1F* h1;
-  TH1F* h2;
-  TH1F* h3;
+  const int NHISTOS=3;
+  TH1F* h[NHISTOS];
 
   char tempName[300];
   if(var2 ==  "" )var2=var1;
   if(var3 ==  "" )var3=var1;
   
   // first get the histogram files
-  TFile *fmc1 = TFile::Open(mcfile1.data());
+  TFile *fmc1   = TFile::Open(mcfile1.data());
   TFile *fmc2   = TFile::Open(mcfile2.data());
   TFile *fmc3   = TFile::Open(mcfile3.data());
 
-  h1  = (TH1F*)(fmc1->Get(var1.data()));
-  h2    = (TH1F*)(fmc2->Get(var2.data()));
-  h3    = (TH1F*)(fmc3->Get(var3.data()));
+  h[0]    = (TH1F*)(fmc1->Get(var1.data()));
+  h[1]    = (TH1F*)(fmc2->Get(var2.data()));
+  h[2]    = (TH1F*)(fmc3->Get(var3.data()));
 
-  TH1D* hscale1_3 =(TH1D*) h1->Clone("hscale1_3");
-  hscale1_3->SetYTitle(Form("Ratio to %s",mcName3.data()));
+  TH1D* hscale[NHISTOS];
 
-  TH1D* hscale2_3 =(TH1D*) h1->Clone("hscale2_3");
-  hscale2_3->SetYTitle(Form("Ratio to %s",mcName3.data()));
+  int COLOR[NHISTOS]={4,2,kOrange-1};
+  int MARKERSTYLE[NHISTOS]={24,21,29};
 
-  h1->GetXaxis()->SetNdivisions(5);
-  h1->GetYaxis()->SetDecimals();
+  for(int i=0; i < NHISTOS; i++){
 
-  h2->GetXaxis()->SetNdivisions(5);
-  h2->GetYaxis()->SetDecimals();
+    hscale[i]   =(TH1D*) h[0]->Clone(Form("hscale%02i",i));
+    hscale[i]   ->SetYTitle(Form("Ratio to %s",mcName3.data()));
+    
+    hscale[i]   ->GetXaxis()->SetNdivisions(5);
+    hscale[i]   ->GetYaxis()->SetDecimals();
 
-  h3->GetXaxis()->SetNdivisions(5);
-  h3->GetYaxis()->SetDecimals();
+    hscale[i]->SetLineColor(COLOR[i]);
+    hscale[i]->SetMarkerColor(COLOR[i]);
+    hscale[i]->SetMarkerStyle(MARKERSTYLE[i]);
 
-  hscale1_3->GetXaxis()->SetNdivisions(5);
-  hscale1_3->GetYaxis()->SetDecimals();
+    hscale[i]->SetTitle("");
+    hscale[i]->SetMaximum(2.3);
+    hscale[i]->SetMinimum(0.1);
+    hscale[i]->SetTitleOffset(1.2,"Y");
 
-  hscale2_3->GetXaxis()->SetNdivisions(5);
-  hscale2_3->GetYaxis()->SetDecimals();
+    h[i]->SetTitle("");
+    h[i]->GetXaxis()->SetNdivisions(5);
+    h[i]->GetYaxis()->SetDecimals();
 
+    h[i]->SetLineColor(COLOR[i]);
+    h[i]->SetMarkerColor(COLOR[i]);
+    h[i]->SetMarkerSize(1);
+    h[i]->SetMarkerStyle(MARKERSTYLE[i]);
+    h[i]->SetTitleOffset(1.2,"Y");
 
-  int COLOR[3]={4,2,kOrange-1};
-  int MARKERSTYLE[3]={24,21,29};
-
-  h1->SetLineColor(COLOR[0]);
-  h1->SetMarkerColor(COLOR[0]);
-  h1->SetMarkerSize(1);
-  h1->SetMarkerStyle(MARKERSTYLE[0]);
-
-
-  h2->SetLineColor(COLOR[1]);
-  h2->SetMarkerColor(COLOR[1]);
-  h2->SetMarkerSize(1);
-  h2->SetMarkerStyle(MARKERSTYLE[1]);
-
-  h3->SetLineColor(COLOR[2]);
-  h3->SetMarkerColor(COLOR[2]);
-  h3->SetMarkerSize(1);
-  h3->SetMarkerStyle(MARKERSTYLE[2]);
+  }
 
   // if normalizing to the same area, set the scale 
 
   int binLo = -1;
   int binHi = -1;
-  int nbins = h1->GetNbinsX();
+  int nbins = h[0]->GetNbinsX();
   if(xmin>-9999.0 && xmax>-9999.0)
     {
 
-      binLo = h1->FindBin(xmin);
-      binHi = h1->FindBin(xmax)-1;
+      binLo = h[0]->FindBin(xmin);
+      binHi = h[0]->FindBin(xmax)-1;
 
     }
 
@@ -89,71 +82,87 @@ void compareGenMore(std::string mcfile1, std::string mcfile2, std::string mcfile
     {
       binLo = 1;
       binHi = nbins;
-      xmin = h1->GetBinLowEdge(1);
-      xmax = h1->GetBinLowEdge(nbins+1);
+      xmin = h[0]->GetBinLowEdge(1);
+      xmax = h[0]->GetBinLowEdge(nbins+1);
     }
 
 
-//   cout << "binLo = " << binLo << ", binHi = " << binHi << endl;
-//    cout << "xmin = " << xmin << "xmax = " << xmax << endl;
+  float scaleFactor[NHISTOS]={1};
 
-  float scale_mc2= (float)h1->Integral(binLo,binHi)/(float)h2->Integral(binLo,binHi);
-
-  h2->Sumw2();
-  h2->Scale(scale_mc2);
-
-  float scale_mc3= (float)h1->Integral(binLo,binHi)/(float)h3->Integral(binLo,binHi);
-
-  h3->Sumw2();
-  h3->Scale(scale_mc3);
-
-  cout << "h1 integral = "   << h1->Integral() << endl;;
-  cout << "h2 integral = "   << h2->Integral() << endl;
-  cout << "h3 integral = "   << h3->Integral() << endl;
-
-  // get the ratio
-  for(int i=1;i<= nbins;i++){
-
-    double nref=h3->GetBinContent(i);
-    double nreferr=h3->GetBinError(i);
-
-    double nmc1=h1->GetBinContent(i); 
-    double nmc1err=h1->GetBinError(i); 
-
-    double nmc2=h2->GetBinContent(i); 
-    double nmc2err=h2->GetBinError(i); 
-
+  for(int ih=1; ih < NHISTOS; ih++){
     
-    if(nref<=0 || nmc1<=0 || nmc2<=0)continue;    
-    
-    if(nreferr<=0 || nmc1err<=0 || nmc2err<=0)
-      continue;
-
-    // now calculate the ratio 1
-    cout << "Bin " << i << " ratio1 = " << nmc1/nref << "\t";
-    double err1_3 = 0;
-    err1_3 =
-      (nmc1/nref)*sqrt(pow(nreferr/nref,2)+pow(nmc1err/nmc1,2));
-    hscale1_3->SetBinContent(i,nmc1/nref);
-    hscale1_3->SetBinError(i,err1_3);
-
-    // now calculate the ratio 2
-    cout << nmc2/nref << endl;
-    double err2_3 = 0;
-    err2_3 =
-      (nmc2/nref)*sqrt(pow(nreferr/nref,2)+pow(nmc2err/nmc2,2));
-    hscale2_3->SetBinContent(i,nmc2/nref);
-    hscale2_3->SetBinError(i,err2_3);
+    float integral = h[ih]->Integral(binLo,binHi);
+    scaleFactor[ih] = integral > 0? (float)h[0]->Integral(binLo,binHi)/integral: 1;
+    h[ih]->Sumw2();
+    h[ih]->Scale(scaleFactor[ih]);
 
   }
 
-  h1->GetXaxis()->SetRangeUser(xmin,xmax);
-  h2->GetXaxis()->SetRangeUser(xmin,xmax);
-  h3->GetXaxis()->SetRangeUser(xmin,xmax);
-  hscale1_3->GetXaxis()->SetRangeUser(xmin,xmax);
-  hscale2_3->GetXaxis()->SetRangeUser(xmin,xmax);
+  for(int ih=0; ih < NHISTOS; ih++)
+    cout << "histogram " << ih << " integral = " << h[ih]->Integral() << endl;
 
 
+  // get the ratio
+
+  for(int ih=0; ih < NHISTOS-1; ih++){
+    cout << "===================================================" << endl;
+    cout << "For histogram " << ih << endl;
+    for(int ib=1;ib<= nbins;ib++){
+
+      double nref    =h[NHISTOS-1]->GetBinContent(ib);
+      double nreferr =h[NHISTOS-1]->GetBinError(ib);
+
+      double ncomp   =h[ih]->GetBinContent(ib); 
+      double ncomperr=h[ih]->GetBinError(ib); 
+
+      double ratio = -9999;
+      double err   = 1e-4;
+
+      hscale[ih]->SetBinContent(ib,ratio);
+      hscale[ih]->SetBinError(ib,err);
+    
+      if(ncomp<=0 || nref<=0)continue;    
+    
+      if(ncomperr<=0 || nreferr<=0)
+	continue;
+      
+      // now calculate the ratio 1
+      ratio = ncomp/nref;
+      err =
+	(ratio)*sqrt(pow(nreferr/nref,2)+pow(ncomperr/ncomp,2));
+
+      hscale[ih]->SetBinContent(ib,ratio);
+      hscale[ih]->SetBinError(ib,err);
+
+      cout << "Bin " << ib << " ratio = " << ratio << " +- " << err << endl;
+
+      cout << "===================================================" << endl;
+    } // end of loop over bins
+  } // end of loop over histograms
+
+  vector<float> maxArray;
+  maxArray.clear();
+
+  for(int ih=0; ih < NHISTOS; ih++){
+
+    h[ih]->GetXaxis()->SetRangeUser(xmin,xmax);
+    hscale[ih]->GetXaxis()->SetRangeUser(xmin,xmax);
+    float max_this  = h[ih]->GetBinError(h[ih]->GetMaximumBin()) + h[ih]->GetMaximum();
+    maxArray.push_back(max_this);
+
+  }
+
+
+  float max = *max_element(maxArray.begin(),maxArray.end());
+  cout << "Max = " << max << endl;
+
+  for(int ih=0; ih < NHISTOS; ih++){
+
+    h[ih]->SetMaximum(1.1*max);
+
+  }
+
+  cout << "here" << endl;
   TCanvas* c1 = new TCanvas("c1","",700,1000);  
   c1->Divide(1,2,0.01,0);
   c1->cd(1);
@@ -164,26 +173,16 @@ void compareGenMore(std::string mcfile1, std::string mcfile2, std::string mcfile
   gPad->SetRightMargin(0.04);
 
   
-  float max_mc1   = h1->GetBinError(h1->GetMaximumBin()) + h1->GetMaximum();
-  float max_mc2   = h2->GetBinError(h2->GetMaximumBin()) + h2->GetMaximum();
-  float max_mc3   = h3->GetBinError(h3->GetMaximumBin()) + h3->GetMaximum();
+  cout << "here1" << endl;
+  h[0]->Draw("e");
+  for(int ih=0; ih < NHISTOS-1; ih++)
+    h[ih]->Draw("esame");
 
+  cout << "here2" << endl;
 
-  vector<float> maxArray;
-  maxArray.push_back(max_mc1);
-  maxArray.push_back(max_mc2);
-  maxArray.push_back(max_mc3);
+  h[NHISTOS-1]->Draw("hesame");
 
-  float max = *max_element(maxArray.begin(),maxArray.end());
-  cout << "Max = " << max << endl;
-
-  h1->SetMaximum(1.1*max);
-  h2->SetMaximum(1.1*max);
-  h3->SetMaximum(1.1*max);
-
-  h1->Draw("e");
-  h2->Draw("esame");
-  h3->Draw("hesame");
+  cout << "here3" << endl;
 
   float x1NDC = 0.735894;
   float y1NDC = 0.620155;
@@ -197,9 +196,9 @@ void compareGenMore(std::string mcfile1, std::string mcfile2, std::string mcfile
   leg->SetTextSize(0.04);
   leg->SetBorderSize(0);
   leg->SetHeader(headertitle.data());
-  leg->AddEntry(h1, mcName1.data());
-  leg->AddEntry(h2, mcName2.data());
-  leg->AddEntry(h3, mcName3.data());
+  leg->AddEntry(h[0], mcName1.data());
+  leg->AddEntry(h[1], mcName2.data());
+  leg->AddEntry(h[2], mcName3.data());
   leg->Draw("same");
 
 
@@ -215,32 +214,22 @@ void compareGenMore(std::string mcfile1, std::string mcfile2, std::string mcfile
   gPad->SetBottomMargin(0.2);
   gPad->SetTickx();
   gStyle->SetOptFit(1);
-  hscale1_3->SetTitle("");
-  hscale1_3->SetMaximum(3.0);
-  hscale1_3->SetMinimum(0.1);
-  hscale1_3->SetTitleOffset(1.2,"Y");
-  hscale1_3->SetLineColor(COLOR[0]);
-  hscale1_3->SetMarkerColor(COLOR[0]);
-  hscale1_3->SetMarkerStyle(MARKERSTYLE[0]);
-  hscale1_3->Draw("e1");
 
-  hscale2_3->SetTitle("");
-  hscale2_3->SetMaximum(3.0);
-  hscale2_3->SetMinimum(0.1);
-  hscale2_3->SetTitleOffset(1.2,"Y");
-  hscale2_3->SetLineColor(COLOR[1]);
-  hscale2_3->SetMarkerColor(COLOR[1]);
-  hscale2_3->SetMarkerStyle(MARKERSTYLE[1]);
-  hscale2_3->Draw("e1same");
+  cout << "here4" << endl;
 
+  hscale[0]->Draw("e1");
+  for(int ih=0; ih < NHISTOS-1; ih++){
+    hscale[ih]->Draw("e1same");
+  }
 
+  cout << "here5" << endl;
   TLine* l2 = new TLine(xmin,1.,xmax,1.);
   l2->SetLineColor(4);
   l2->SetLineStyle(3);
   l2->Draw("same");
 
 
-  string dirName = "compareGen";
+  string dirName = "compareGenMore";
   gSystem->mkdir(dirName.data());
 
   std::string filename;
