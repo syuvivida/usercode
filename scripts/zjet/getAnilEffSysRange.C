5158926cf25318fec7439994d14d5d7a1c768938
syu <>
2012-09-12 15:30:28 +0000

    macro to get efficiency systematic uncertainty from Anil's root file

---
+++ scripts/zjet/getAnilEffSysRange.C
@@ -0,0 +1,192 @@
+#include "/afs/cern.ch/user/s/syu/scripts/histoLib.h"
+
+void getAnilEffSysRange(std::string var, bool update=false, double xmin=-9999.0, double xmax=-9999.0)
+{
+  TH1D* hcentral;
+  TH1D* hup;
+  TH1D* hdown;
+
+  std::string anilName;
+  std::string xtitle;
+
+  if(var=="h_ystar")
+    {
+      anilName = "EfficienyVsYdif";
+      xtitle = "0.5|Y_{Z}-Y_{jet}|";	    
+    }
+  else if(var=="h_yB")
+    {
+      anilName = "EfficienyVsYsum";
+      xtitle = "0.5|Y_{Z}+Y_{jet}|";
+    }
+  else if(var=="h_jety")
+    {
+      anilName = "EfficienyVsYjet";
+      xtitle = "|Y_{jet}|";
+    }
+  else if(var=="h_zy")
+    {
+      anilName ="EfficienyVsYz";
+      xtitle = "|Y_{Z}|";
+    }
+ 
+  // first get the histogram files
+  TFile *fmc  = TFile::Open("/afs/cern.ch/work/s/syu/anil_rootfile/"
+			    "EffiZCut_Jer0.root");  
+  cout << "Opening " << fmc->GetName() << endl;
+
+  std::string var1=var;
+  std::string remword1 ="h_";
+  size_t pos1  = var1.find(remword1);
+  if(pos1!= std::string::npos)
+    var1.replace(pos1,remword1.length(),"");
+
+  hcentral    = (TH1D*)(fmc->Get(anilName.data()));
+  hcentral    -> SetName(Form("hcentral_%s",var1.data()));
+  hcentral    -> SetXTitle(xtitle.data());
+  hcentral    -> Sumw2();
+
+  TFile *fup  = TFile::Open("/afs/cern.ch/work/s/syu/anil_rootfile/"
+			    "EffiZCut_Jer0_sfUp.root");  
+  cout << "Opening " << fup->GetName() << endl;
+
+  hup         = (TH1D*)(fup->Get(anilName.data()));
+  hup         -> SetName(Form("hup_%s",var1.data()));
+  hup         -> SetXTitle(xtitle.data());
+  hup         -> Sumw2();
+
+  TFile *fdown  = TFile::Open("/afs/cern.ch/work/s/syu/anil_rootfile/"
+			    "EffiZCut_Jer0_sfDn.root");  
+  cout << "Opening " << fdown->GetName() << endl;
+
+  hdown         = (TH1D*)(fdown->Get(anilName.data()));
+  hdown         -> SetName(Form("hdown_%s",var1.data()));
+  hdown         -> SetXTitle(xtitle.data());
+  hdown         -> Sumw2();
+
+
+  int binLo = -1;
+  int binHi = -1;
+  int nbins = hcentral->GetNbinsX();
+  if(xmin>-9999.0 && xmax>-9999.0)
+    {
+
+      binLo = hcentral->FindBin(xmin);
+      binHi = hcentral->FindBin(xmax)-1;
+
+    }
+
+  else
+    {
+      binLo = 1;
+      binHi = nbins;
+      xmin = hcentral->GetBinLowEdge(1);
+      xmax = hcentral->GetBinLowEdge(nbins+1);
+    }
+
+
+  cout << "Before scaling = " << endl;
+  cout << "Integral of hcentral = " << hcentral->Integral(binLo, binHi) << endl;  cout << "Integral of hup = " << hup->Integral(binLo, binHi) << endl;
+  cout << "Integral of hdown = " << hdown->Integral(binLo, binHi) << endl;
+
+  double scale_central=1.0/hcentral->Integral(binLo, binHi);  
+  double scale_up     =1.0/hup->Integral(binLo, binHi);
+  double scale_down   =1.0/hdown->Integral(binLo, binHi);
+
+
+  cout << "After scaling = " << endl;
+  cout << "Integral of hcentral = " << hcentral->Integral(binLo, binHi) << endl;  cout << "Integral of hup = " << hup->Integral(binLo, binHi) << endl;
+  cout << "Integral of hdown = " << hdown->Integral(binLo, binHi) << endl;
+
+  TH1D* r_corr =(TH1D*) hcentral->Clone(Form("r_%s_corr",var1.data()));
+  r_corr->SetXTitle(xtitle.data());
+
+  // note, unlike the other uncertainty, here we are showing the difference, 
+  // not the ratio
+  TH1D* r_up   =(TH1D*) hcentral->Clone(Form("r_%s_up",var1.data()));
+  r_up->Reset();
+  r_up->SetXTitle(xtitle.data());
+
+  TH1D* r_down   =(TH1D*) hcentral->Clone(Form("r_%s_down",var1.data()));
+  r_down->Reset();
+  r_down->SetXTitle(xtitle.data());
+
+  for(int ib=1; ib<= r_corr->GetNbinsX(); ib++){
+
+    double upDiff = hup->GetBinContent(ib)*scale_up - 
+      hcentral->GetBinContent(ib)*scale_central;
+
+    double dnDiff = hdown->GetBinContent(ib)*scale_down - 
+      hcentral->GetBinContent(ib)*scale_central;
+
+    double statErr = hcentral->GetBinError(ib);
+
+    r_up  ->SetBinContent(ib, sqrt(upDiff*upDiff+ statErr*statErr));
+
+    r_down->SetBinContent(ib, sqrt(dnDiff*dnDiff+ statErr*statErr));
+
+  }
+
+
+  double minCorr  =  99999.0;
+  double maxCorr  = -99999.0;
+
+  double minSysUp =  99999.0;
+  double maxSysUp = -99999.0;
+  double minSysDn =  99999.0;
+  double maxSysDn = -99999.0;
+
+  cout << "binLo = " << binLo << "\t binHi = " << binHi << endl;
+  
+  for(int i=binLo; i <= binHi; i++){
+    
+    if(r_up->GetBinContent(i)<1e-6)continue;
+    if(r_down->GetBinContent(i)<1e-6)continue;
+
+    double tempcorr = r_corr->GetBinContent(i);
+    if(tempcorr < minCorr)minCorr=tempcorr;
+    if(tempcorr > maxCorr)maxCorr=tempcorr;
+
+    double tempUp = r_up->GetBinContent(i);
+    double tempDn = r_down->GetBinContent(i);
+    
+    if(tempUp < minSysUp)minSysUp=tempUp;
+    if(tempUp > maxSysUp)maxSysUp=tempUp;
+
+    if(tempDn < minSysDn)minSysDn=tempDn;
+    if(tempDn > maxSysDn)maxSysDn=tempDn;
+
+  }
+
+  cout << "The range of correction for " << var << " is " << 
+    fabs(maxCorr-minCorr)*100 << " % " << endl;
+
+  cout << "The range of systematic uncertainty for up " << var << " is " << 
+    minSysUp*100 << " %" << " -- " << maxSysUp*100 << " %" << endl;
+  cout << "The total range of systematic uncertainty for up " << var << " is " << 
+    fabs(maxSysUp-minSysUp)*100 << " %" << endl;
+
+  cout << "The range of systematic uncertainty for down " << var << " is " << 
+    minSysDn*100 << " %" << " -- " << maxSysDn*100 << " %" << endl;
+  cout << "The total range of systematic uncertainty for down " << var << " is " << 
+    fabs(maxSysDn-minSysDn)*100 << " %" << endl;
+
+  cout << endl;
+
+  
+  std::string command = "recreate";
+  if(update)command = "update";
+  TFile* outFile = new TFile(Form("anilEffSys.root"),
+			     command.data());       
+  r_down->Write();
+  r_up->Write();
+  r_corr->Write();
+
+  hcentral->Write();
+  hup->Write();
+  hdown->Write();
+
+  outFile->Close();
+
+}
+		     
