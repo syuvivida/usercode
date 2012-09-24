c9e56fde79b29f6145959fe04d773819c6d84675
syu <>
2012-09-24 12:58:45 +0000

    include correlation between different bins due to JES

--- scripts/zjet/bigmatrix_corr.C
+++ scripts/zjet/bigmatrix_corr.C
@@ -23,6 +23,7 @@ void dumpElements(TMatrixD& a)
     cout << "determinent = " << a.Determinant() << endl;
   a.Print();
   cout << endl << endl;
+
   return;
 }
 
@@ -32,16 +33,19 @@ void dumpElements(TVectorD& a)
   cout << endl << endl;
   a.Print();
   cout << endl << endl;
+
   return;
 }
 
 //==========================================================================
 //
-//  Main function
+//  Main function 
+//  This method takes into account the correlation between different 
+//  bins due to JES
 //
 //==========================================================================
 
-void jety_bigmatrix(
+void bigmatrix_corr(std::string eikoName="h_jety",
 		    bool update=false,
 		    bool acceptanceCorr=true,
 		    double correlation=1.0, 
@@ -64,48 +68,11 @@ void jety_bigmatrix(
   TH1D* h_combine; // for combining electron and muon channels  
   TH1D* h_corr; // for correction of acceptance
 
-
-  TMatrixD covEle(15,15);
-  TMatrixD covMuo(15,15);
-
-  TMatrixD covEle_obj(15,15);
-  TMatrixD covMuo_obj(15,15);
-
-  TFile f_cove("mainCore/electron_covMatrix_Bayes.root");
-  if (f_cove.IsZombie()) {
-    cout << endl << "Error opening file" << f_cove.GetName() << endl;
-    return;
-  }
-  else
-    cout << endl << "Opened " << f_cove.GetName() << endl << endl;
- 
-  covEle =*((TMatrixD*)(f_cove.Get("covMatrix_Bayes_Yjet")));
-  covEle.Print();
-  
-  TFile f_covm("mainCore/muon_covMatrix_jety.root");
-  if (f_covm.IsZombie()) {
-    cout << endl << "Error opening file" << f_covm.GetName() << endl;
-    return;
-  }
-  else
-    cout << endl << "Opened " << f_covm.GetName() << endl << endl;
- 
-  covMuo =*((TMatrixD*)(f_covm.Get("Bayes_YDiff")));
-  covMuo.Print();
-
-
-  //===========================================================
-  // electron channel
-  //===========================================================
-
-
-  std::string eikoName="h_jety";
   std::string remword3  ="h_";
   std::string corrName = eikoName;
   size_t pos3  = corrName.find(remword3);
   if(pos3!= std::string::npos)
     corrName.replace(pos3,remword3.length(),"");
-
 
   // acceptance correction for electrons
   TFile f_crack("mainCore/ave_sherpamadgraph.root");
@@ -144,10 +111,43 @@ void jety_bigmatrix(
   std::string effElectronName;
   std::string effMuonName;
 
-  
-  effElectronName = "EfficienyVsYjet";
-  effMuonName = "EffCorr_Yjet";
+  if(eikoName=="h_ystar")
+    {
+      xtitle = "0.5|Y_{Z}-Y_{jet}|";
+      muName = "YDiff";
+      kengName = "DEta_per2_Z1jets_BE";
+      
+      effElectronName = "EfficienyVsYdif";
+      effMuonName = "EffCorr_Ydiff";
+    }
 
+  else if(eikoName=="h_yB")
+    {
+      xtitle = "0.5|Y_{Z}+Y_{jet}|";
+      muName = "YSum";
+      kengName = "SumEta_per2_Z1jets_BE";
+
+      effElectronName = "EfficienyVsYsum";
+      effMuonName = "EffCorr_Ysum";
+    }
+  else if(eikoName=="h_jety")
+    {
+      xtitle = "|Y(jet)|";
+      muName = "Yjet";
+      kengName = "Z1jets_1jeta_BE";
+
+      effElectronName = "EfficienyVsYjet";
+      effMuonName = "EffCorr_Yjet";
+    }
+  else if(eikoName=="h_zy")
+    {
+      xtitle = "|Y(Z)|";
+      muName = "YZ";
+      kengName ="dimuoneta1jet_BE";
+
+      effElectronName = "EfficienyVsYz";
+      effMuonName = "EffCorr_Yz";
+    }
 
   TFile f_eff_electron("mainCore/EffiZCut_Jer0.root");
   if(f_eff_electron.IsZombie()){
@@ -173,7 +173,6 @@ void jety_bigmatrix(
   heff_mu -> SetName(Form("heff_mu_%s",corrName.data()));
 
 
-  xtitle = "|Y(jet)|";
 
 
   //===========================================================
@@ -192,27 +191,6 @@ void jety_bigmatrix(
   h_e  -> SetTitle("");
   h_e  -> Sumw2();
 
-  h_e  -> Reset();
-
-  double darko[12][2]={
-    {13218.9 , 155.4},
-    {13038.2 , 159.7},
-    {12338.0 , 155.2},
-    {12004.6 , 153.5},
-    {11173.1 , 150.4},
-    { 9959.5 , 142.6},
-    { 9266.5 , 137.1},
-    { 7725.4 , 126.6},
-    { 6897.7 , 120.1},
-    { 5876.9 , 110.5},
-    { 4814.4 , 101.1},
-    { 3277.8 , 80.8}
-  };
-
-  for(int i=1;i<=12;i++){
-    h_e->SetBinContent(i, darko[i-1][0]);
-    h_e->SetBinError(i, darko[i-1][1]);
-  }
   //-------------------------------------------------------------------
   // use electron channel to count the number of non-zero bins
   // so to avoid singular error matrix
@@ -224,12 +202,7 @@ void jety_bigmatrix(
   //-------------------------------------------------------------------
 
   h_e  -> Divide(h_corr);
-  double scale_electron = 1.0/h_e->Integral();
-
-  covEle_obj = covEle * pow(scale_electron,2);
-  covEle_obj.Print();
-
-  h_e  -> Scale(scale_electron);
+  h_e  -> Scale(1.0/h_e->Integral());
   h_e  -> SetYTitle("Arbitrary Unit");
   h_e  -> SetTitleOffset(2.0,"Y");
   h_e  -> GetYaxis()->SetDecimals();
@@ -292,10 +265,8 @@ void jety_bigmatrix(
   //===========================================================
   // muon channel
   //===========================================================
-  muName = "Bayes_UnfoldedYjet";
-  kengName = "Z1jets_1jeta_BE";
 
-  TFile f_mu("mainCore/Unfolded_Yjet_091912.root");
+  TFile f_mu("mainCore/DoubleMu2011_EffCorr_091812.root");
   if (f_mu.IsZombie()) {
     cout << endl << "Error opening file" << f_mu.GetName() << endl << endl;
     return;
@@ -307,13 +278,7 @@ void jety_bigmatrix(
   h_mu -> Sumw2();
   h_mu -> SetName(Form("h_mu_%s",corrName.data()));
   h_mu  -> SetTitle("");
-
-  double scale_muon = 1.0/h_mu->Integral();
-
-  covMuo_obj = covMuo * pow(scale_muon,2);
-  covMuo_obj.Print();
-
-  h_mu -> Scale(scale_muon);
+  h_mu -> Scale(1.0/h_mu->Integral());
   h_mu -> SetLineColor(kRed-7);
   h_mu -> SetMarkerColor(kRed-7);
   h_mu -> SetMarkerSize(1);
@@ -357,14 +322,16 @@ void jety_bigmatrix(
     for(int icol=0; icol < nbins; icol++)
       U(irow,icol) = ( (irow== icol) || (irow== icol+nbins) )? 1:0;
   // debug
-  dumpElements(U);
+  //   dumpElements(U);
 
   TMatrixD transposeU(nbins,NELE);
   transposeU.Transpose(U);
-  dumpElements(transposeU);
+  //   dumpElements(transposeU);
 
   
   TVectorD measurement(NELE);
+
+
   // jet energy scale for electron channel
   TVectorD jesSys_ele(nbins);
   TVectorD jesSys_muo(nbins);
@@ -378,9 +345,8 @@ void jety_bigmatrix(
 
     double stat_mc_eff_e = heff_e->GetBinContent(ibin+1)<1e-6? 0: 
       value_e*heff_e->GetBinError(ibin+1)/heff_e->GetBinContent(ibin+1);
-
-    
-    double stat_e  = sqrt( covEle_obj(ibin,ibin) + 
+  
+    double stat_e  = sqrt( pow(h_e->GetBinError(ibin+1),2) + 
 			   pow(stat_mc_eff_e,2));
 
     double value_e_jes = h_ejes->GetBinContent(ibin+1);
@@ -407,10 +373,10 @@ void jety_bigmatrix(
 
     double stat_mc_eff_m = heff_mu->GetBinContent(ibin+1)<1e-6? 0: 
       value_m*heff_mu->GetBinError(ibin+1)/heff_mu->GetBinContent(ibin+1);
-
-    double stat_m  = sqrt( covMuo_obj(ibin,ibin) + 
-			   pow(stat_mc_eff_m,2));
     
+    double stat_m  = sqrt( pow(h_mu->GetBinError(ibin+1),2) + 
+			   pow(stat_mc_eff_m,2));
+
     double value_m_jes = h_mujes->GetBinContent(ibin+1);
     if(value_m_jes <1e-10)continue;
 
@@ -440,46 +406,42 @@ void jety_bigmatrix(
     errorM(ibin,nbins+ibin) = errorM(nbins+ibin,ibin) = 
       correlation*sys_e*sys_m;
 
-  } // loop over the number of bins
-  
-  errorM.Print();
 
-  // re-assign errorM for the correlation due to JES
+  } // loop over the number of bins
+
+
+  // debug
+
+  dumpElements(jesSys_ele);
+  dumpElements(jesSys_muo);
+  // print out measurement value  
+  //   dumpElements(measurement);
+
+  //   // print out error matrix component
+  dumpElements(errorM);
+
+
+  // assign the correlation between different bins due to JES
+  // 100% correlation
   for(int ibin=0; ibin < nbins; ibin++){
     for(int jbin=0; jbin<nbins; jbin++)
       {
-	if(jbin!=ibin){
-	  //     // for now 
-	  //     // the correlation between different bins for different lepton channels
-	  //     // channel is zero
-
-	  // 	  errorM(ibin,jbin)=covEle_obj(ibin,jbin);
-	  // 	  errorM(nbins+ibin,nbins+jbin)=covMuo_obj(ibin,jbin);
-	  // 	  errorM(ibin,nbins+jbin)=0; // electron-muon channel
-	  // 	  errorM(nbins+ibin,jbin)=0; // muon-electron channel
-
+	if(jbin!=ibin){	
 	  // electron channel
-	  errorM(ibin,jbin)= correlation*jesSys_ele(ibin)*jesSys_ele(jbin)
-	    + covEle_obj(ibin,jbin);   
+	  errorM(ibin,jbin)= correlation*jesSys_ele(ibin)*jesSys_ele(jbin);   
 	  // muon channel
 	  errorM(nbins+ibin,nbins+jbin)= correlation*jesSys_muo(ibin)*
-	    jesSys_muo(jbin) + covMuo_obj(ibin,jbin);
+	    jesSys_muo(jbin);
 	  
 	  // electron-muon channel
 	  errorM(ibin,nbins+jbin)= correlation*jesSys_ele(ibin)*jesSys_muo(jbin);
 	  // muon-electron channel
 	  errorM(nbins+ibin,jbin)= correlation*jesSys_muo(ibin)*jesSys_ele(jbin); 
-
 	}	
       }
   }
-  // debug
-  // print out measurement value  
-  dumpElements(measurement);
 
-  //   // print out error matrix component
   dumpElements(errorM);
-
 
   TMatrixD errorInverse = errorM;
   double* det;
@@ -492,30 +454,30 @@ void jety_bigmatrix(
 
   TMatrixD matrixRight(nbins,NELE);  
   matrixRight = transposeU*errorInverse;
-  dumpElements(matrixRight);
+  //   dumpElements(matrixRight);
 
   TMatrixD matrixLeft(nbins,nbins);
   matrixLeft = transposeU*(errorInverse*U);
-  dumpElements(matrixLeft);
+  //   dumpElements(matrixLeft);
 
   TMatrixD matrixLeftInverse = matrixLeft;
   double* det2;
   matrixLeftInverse.Invert(det2);
-  dumpElements(matrixLeftInverse);
+  //   dumpElements(matrixLeftInverse);
 
   TMatrixD lambda(nbins,NELE);
   lambda= matrixLeftInverse*matrixRight;
-  dumpElements(lambda);
+  //   dumpElements(lambda);
   
   TMatrixD transposeLambda(NELE,nbins);
   transposeLambda.Transpose(lambda);
-  dumpElements(transposeLambda);
+  //   dumpElements(transposeLambda);
 
-  dumpElements(lambda);
+  //   dumpElements(lambda);
 
   TVectorD combined_value(nbins);
   combined_value = lambda*measurement;
-  dumpElements(combined_value);
+  //   dumpElements(combined_value);
 
   TMatrixD combined_error(nbins,nbins);
   combined_error = lambda*(errorM*transposeLambda);
@@ -553,7 +515,7 @@ void jety_bigmatrix(
 
   std::string command = "recreate";
   if(update)command="update";
-  TFile* outFile = new TFile("jety_bigmatrix.root", command.data());
+  TFile* outFile = new TFile("bigmatrix_corr.root", command.data());
   h_e      ->Write();
   h_mu     ->Write();
   h_combine->Write();
@@ -598,4 +560,5 @@ void jety_bigmatrix(
   c1->Print(Form("%s.eps",fileName.data()));
   c1->Print(Form("%s.gif",fileName.data()));
   c1->Print(Form("%s.pdf",fileName.data()));
+
 }
