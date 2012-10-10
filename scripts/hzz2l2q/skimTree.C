2505b981cda594105b02a7a1e1b5f2a6cd06d20e
syu <>
2012-10-10 20:34:40 +0000

    add trigger filter for data

--- scripts/hzz2l2q/skimTree.C
+++ scripts/hzz2l2q/skimTree.C
@@ -18,7 +18,6 @@ class runInfo
 public:
   int run;
   int evt;
-
   bool operator==( const runInfo& other ) const 
   {
     return ((this->run== other.run) && 
@@ -63,14 +62,28 @@ void skimTree(std::string inputFile_)
   Int_t           eventNo=-1;
   Int_t           runNo=-1;
 
+  vector<int>*     trigResults;
+  vector<string>*  trigName;
+
+
+  bool isData=false;
+  if(forOutput.find("DoubleElectron")!= std::string::npos || forOutput.find("DoubleMu")!= std::string::npos)
+    isData=true;
+
+
   fChain->SetBranchAddress("EvtInfo_EventNum",&eventNo);
   fChain->SetBranchAddress("EvtInfo_RunNum",&runNo);
+  if(isData){
+    fChain->SetBranchAddress("trigResults", &trigResults);
+    fChain->SetBranchAddress("trigName", &trigName);
+  }
 
   Long64_t nlines=0;
 
   vector<runInfo> myList;
 
   ifstream fin;
+
   if(forOutput.find("DoubleElectron")!= std::string::npos)
     fin.open("/data4/syu/52X_533_validation/DoubleElectron_common.txt");
   else if(forOutput.find("DoubleMu")!= std::string::npos)
@@ -95,11 +108,53 @@ void skimTree(std::string inputFile_)
   Long64_t nPassEvt=0;
 
   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+
     
     eventNo = -1;
     runNo = -1;
-
+    trigName = 0;
+    trigResults = 0;
+    
     fChain->GetEntry(jentry);
+
+
+    bool passTrigger=false;
+    if(isData){
+
+      for(int it=0; it< trigName->size(); it++)
+	{
+	  std::string thisTrig= trigName->at(it);
+	  int results = trigResults->at(it);
+//   	  cout << thisTrig << "\t" << results << endl;
+
+	  if(forOutput.find("DoubleElectron")!= std::string::npos && 
+	     thisTrig.find("HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL")
+	     != std::string::npos && results==1)
+	    {
+	      passTrigger=true;
+// 	      cout << "Find HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL" << endl;
+	      break;
+	    }	  
+
+	  if(forOutput.find("DoubleMu")!= std::string::npos && thisTrig.find("HLT_Mu17_Mu8")!= std::string::npos && results==1)
+	    {
+	      passTrigger=true;
+// 	      cout << "Find HLT_Mu17_Mu8" << endl;
+	      break;
+	    }
+
+	  if(forOutput.find("DoubleMu")!= std::string::npos && thisTrig.find("HLT_Mu17_TkMu8")!= std::string::npos && results==1)
+	    {
+	      passTrigger=true;
+// 	      cout << "Find HLT_Mu17_TkMu8" << endl;
+	      break;
+	    }
+
+	}
+
+    }
+    if(!passTrigger && isData)continue;
+
     bool pass=false;
 
     runInfo thisInfo;
