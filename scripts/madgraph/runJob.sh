62822d9d2de4f0e21b361c8b8c083cea5ffe4e90
syu <>
2012-12-27 14:35:45 +0000

    updated

--- scripts/madgraph/runJob.sh
+++ scripts/madgraph/runJob.sh
@@ -6,5 +6,5 @@ setenv SCRAM_ARCH slc5_amd64_gcc462; eval `scramv1 runtime -csh`
 
 cd /afs/cern.ch/work/s/syu/madgraph/MG5v1.1
 
-./produceMadgraph.sh $1 10 10000
+./produceMadgraph.sh $1 10 100000
 
