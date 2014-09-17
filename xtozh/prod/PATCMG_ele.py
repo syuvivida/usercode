cacd483cd7f91de9ea68ffd4c0003a0204df8076
Shin-Shan Yu <syu@ncu200.cern.ch>
2014-09-17 10:33:08 +0200

    updated python and cfg files

--- xtozh/prod/PATCMG_ele.py
+++ xtozh/prod/PATCMG_ele.py
@@ -39,11 +39,6 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 10000
 
 process.source = cms.Source("PoolSource",
                             fileNames =cms.untracked.vstring(
-#    'root://xrootd.unl.edu//store/user/caber/AbelianZprime_hZ_qqll_LHC8_M1500_GENSIM_18082013/AbelianZprime_hZ_qqll_LHC8_M1500_20082013_AODSIM/c8f8ed334db8a7d6f56c62266b1dfa5b/HZ_QQLL_AODSIM_11_1_2ep.root'
-#    'root://xrootd.unl.edu//store/data/Run2012C/DoublePhotonHighPt/AOD/22Jan2013-v1/30000/30E6F287-367C-E211-87E8-0024E876993E.root'
-#    'root://xrootd.unl.edu//store/data/Run2012D/DoubleMuParked/AOD/22Jan2013-v1/20000/002E1374-5F84-E211-83C4-20CF305616D0.root'
-#    'root://xrootd.unl.edu//store/data/Run2012A/DoubleMu/AOD/22Jan2013-v1/20000/001AE30A-BA81-E211-BBE7-003048FFD770.root'
-#    'root://xrootd.unl.edu//store/data/Run2012A/Photon/AOD/22Jan2013-v1/20000/007A5A14-1069-E211-8BDD-0025905964B4.root'
     'root://xrootd.unl.edu//store/data/Run2012D/DoublePhotonHighPt/AOD/22Jan2013-v1/20000/000E114D-3985-E211-AF9D-180373FFCED8.root'
 #    'file:/afs/cern.ch/work/s/syu/public/forStudents/GluGluToHToZZTo2L2Q_M-900_8TeV-powheg-pythia6_PU_S10_START53_V7A-v1.root'
     )
@@ -709,22 +704,6 @@ if createCMGtuple:
 ##                         |___/
 
 
-if runOnMC:
-    PlotSuffix = "_MC"
-    jecLevels = [
-        'START53_V23_L1FastJet_AK7PFchs.txt',
-        'START53_V23_L2Relative_AK7PFchs.txt',
-        'START53_V23_L3Absolute_AK7PFchs.txt'
-        ]
-else :
-    jecLevels = [
-        'FT_53_V21_AN4_L1FastJet_AK7PFchs.txt',
-        'FT_53_V21_AN4_L2Relative_AK7PFchs.txt',
-        'FT_53_V21_AN4_L3Absolute_AK7PFchs.txt',
-        'FT_53_V21_AN4_L2L3Residual_AK7PFchs.txt'
-        ]
-            
-
 
 
 from DelPanj.TreeMaker.eSelXZh_cff import *
@@ -732,36 +711,75 @@ from DelPanj.TreeMaker.muSelXZh_global_cff import *
 from DelPanj.TreeMaker.muSelXZh_tracker_cff import *
 
 
+if runOnMC:
+    CA8jecUnc='START53_V23_Uncertainty_AK7PFchs.txt'
+    CA8jecLevels = [
+        'START53_V23_L1FastJet_AK7PFchs.txt',
+        'START53_V23_L2Relative_AK7PFchs.txt',
+        'START53_V23_L3Absolute_AK7PFchs.txt'
+        ]
+    AK5jecUnc='START53_V23_Uncertainty_AK5PFchs.txt'
+    AK5jecLevels = [
+        'START53_V23_L1FastJet_AK5PFchs.txt',
+        'START53_V23_L2Relative_AK5PFchs.txt',
+        'START53_V23_L3Absolute_AK5PFchs.txt'
+        ]
+else :
+    CA8jecUnc='FT_53_V21_AN4_Uncertainty_AK7PFchs.txt'
+    CA8jecLevels = [
+        'FT_53_V21_AN4_L1FastJet_AK7PFchs.txt',
+        'FT_53_V21_AN4_L2Relative_AK7PFchs.txt',
+        'FT_53_V21_AN4_L3Absolute_AK7PFchs.txt',
+        'FT_53_V21_AN4_L2L3Residual_AK7PFchs.txt'
+        ]
+    AK5jecUnc='FT_53_V21_AN4_Uncertainty_AK5PFchs.txt'
+    AK5jecLevels = [
+        'FT_53_V21_AN4_L1FastJet_AK5PFchs.txt',
+        'FT_53_V21_AN4_L2Relative_AK5PFchs.txt',
+        'FT_53_V21_AN4_L3Absolute_AK5PFchs.txt',
+        'FT_53_V21_AN4_L2L3Residual_AK5PFchs.txt'
+        ]
+
 process.tree = cms.EDAnalyzer(
     'TreeMaker',
-    fillPUweightInfo_ = cms.bool(False),
+    fillPUweightInfo_ = cms.bool(True),
     fillEventInfo_ = cms.bool(True),
     fillGenInfo_   = cms.bool(True),
     fillMuonInfo_  = cms.bool(True),
     fillElecInfo_  = cms.bool(True),
     fillJetInfo_   = cms.bool(True),
-    fillMetInfo_   = cms.bool(False),
+    fillMetInfo_   = cms.bool(True),
     fillTrigInfo_  = cms.bool(True),
     fillPhotInfo_  = cms.bool(False),
     genPartLabel=cms.InputTag("genParticles"),
     patMuons=cms.InputTag("patMuonsWithTrigger"),
     patElectrons = cms.InputTag("patElectronsWithTrigger"),
-    Jets=cms.InputTag("selectedPatJetsCA8CHSwithQJetsForBoostedTaus"),
     PrunedJets=cms.InputTag("selectedPatJetsCA8CHSprunedForBoostedTaus"),
     rhoSrc = cms.InputTag('kt6PFJets', 'rho'),
     pvSrc  = cms.InputTag('goodOfflinePrimaryVertices'),
-    jecPayloadNames = cms.vstring( jecLevels ),
-    jecUncName = cms.string('START53_V23_Uncertainty_AK7PFchs.txt'),    
+    ### CA8Jet
+    CA8Jets=cms.InputTag("selectedPatJetsCA8CHSwithQJetsForBoostedTaus"),
+    CA8jecPayloadNames = cms.vstring( CA8jecLevels ),
+    CA8jecUncName = cms.string(CA8jecUnc),    
+    ### AK5Jet
+    AK5Jets=cms.InputTag("patJetsWithVarCHS"),
+    AK5jecPayloadNames = cms.vstring( AK5jecLevels ),
+    AK5jecUncName = cms.string(AK5jecUnc),    
     e2012IDSet  =  eSelXZh,
     globalMuonIDSet = muSelXZh_global,
     trackerMuonIDSet = muSelXZh_tracker,
-    eleRhoIso = cms.InputTag("kt6PFJets","rho"),
-    patMet=cms.InputTag("patMETs"),
+    eleRhoIso = cms.InputTag("kt6PFJets","rho"),  
+    patMetRaw=cms.InputTag("patMETsRaw"),
+    patMet = cms.InputTag("patMetShiftCorrected"),
     outFileName=cms.string('outputFileName.root')
     )
 
+
 if not runOnMC:
     process.tree.fillGenInfo_   = cms.bool(False)
+    process.tree.fillPUweightInfo_ = cms.bool(False)
+
+
 
 if createFlatTuple: 
     process.TFileService = cms.Service("TFileService",
