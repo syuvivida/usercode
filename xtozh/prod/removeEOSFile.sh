011c706cb8e9138460c3e43846b3b9a64880cc71
Shin-Shan Yu <syu@ncu200.cern.ch>
2014-09-18 18:34:48 +0200

    Script to remove EOS directories

---
+++ xtozh/prod/removeEOSFile.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+scriptname=`basename $0`
+EXPECTED_ARGS=1
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "Usage: ./$scriptname directory"
+    echo "Example: ./$scriptname AbelianZPrime_ZH_lljj_M800-MADGRAPH"
+    exit 1
+fi
+
+echo $1
+
+cmsLs /store/user/syu/$1 | grep -a syu | awk '{print "cmsLs "$5}' | bash | grep -a syu | awk '{print "cmsLs "$5}' | bash | grep -a root | awk -v my_var=$1 '{print "cmsRm "$5" temp/"my_var"/."}' | bash
+
+cmsLs /store/user/syu/$1 | grep -a syu | awk '{print "cmsLs "$5}' | bash | grep -a syu | awk '{print "cmsRmdir "$5}' | bash
+
+cmsLs /store/user/syu/$1 | grep -a syu | awk '{print "cmsRmdir "$5}' | bash
+
+cmsRmdir /store/user/syu/$1
