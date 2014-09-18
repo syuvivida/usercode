90213dbc354a3a5e69195b257ad5e7de33effe4f
Shin-Shan Yu <syu@ncu200.cern.ch>
2014-09-18 18:05:49 +0200

    updated for SLC6 running

--- xtozh/DelPanj/TreeMaker/interface/patMetTree.h
+++ xtozh/DelPanj/TreeMaker/interface/patMetTree.h
@@ -40,14 +40,23 @@ class patMetTree : public baseTree{
  private:
   patMetTree();
   
+  edm::InputTag patMetRawLabel_;
   edm::InputTag patMetLabel_;
 
-  std::vector<std::string> patMetType_; 
-  std::vector<Float_t> patMetSumEt_;
-  std::vector<Float_t> patMetPt_;
-  std::vector<Float_t> patMetEta_;
-  std::vector<Float_t> patMetPhi_;
-  std::vector<Float_t> patMetM_;
+  Float_t patMetCorrPt_;  
+  Float_t patMetCorrPhi_; 
+  Float_t patMetCorrSumEt_;
+  Float_t patMetCorrSig_;
+  Float_t patMetRawPt_;
+  Float_t patMetRawPhi_;
+  Float_t patMetRawSumEt_;
+  Float_t patMetRawCov00_;
+  Float_t patMetRawCov01_;
+  Float_t patMetRawCov10_;
+  Float_t patMetRawCov11_;
+
+
+
 };
 
 #endif
