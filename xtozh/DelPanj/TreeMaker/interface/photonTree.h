82e90237cdaff26ba4cb32aa3afb74cf51a7c6cc
syuvivida <syu@phy.ncu.edu.tw>
2015-05-07 11:07:39 +0800

    add photon variables used for 2012 analyes

--- xtozh/DelPanj/TreeMaker/interface/photonTree.h
+++ xtozh/DelPanj/TreeMaker/interface/photonTree.h
@@ -14,8 +14,18 @@ Anil Singh: Empty template created.
 #include "TTree.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "DataFormats/Common/interface/Handle.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+#include "DataFormats/BeamSpot/interface/BeamSpot.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+#include "DataFormats/EgammaCandidates/interface/Photon.h"
+#include "DataFormats/EgammaCandidates/interface/PhotonFwd.h"
 #include "DataFormats/PatCandidates/interface/Photon.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
+#include "DataFormats/EgammaCandidates/interface/GsfElectron.h"
+#include "RecoEgamma/EgammaTools/interface/ConversionFinder.h"
+#include "RecoEgamma/EgammaTools/interface/ConversionTools.h"
+#include "EGamma/EGammaAnalysisTools/src/PFIsolationEstimator.cc"
 #include "DelPanj/TreeMaker/interface/utils.h"
 #include "DelPanj/TreeMaker/interface/baseTree.h"
 
@@ -33,14 +43,30 @@ class photonTree : public baseTree{
 
  private:
   photonTree(){};
-  Bool_t usePFObjects_;
+  edm::InputTag recVtxsLabel_;
   edm::InputTag photonLabel_;
+  edm::InputTag gsfElectronLabel_;
+  edm::InputTag convLabel_;
+  edm::InputTag beamSpotLabel_;
+  edm::InputTag pfCandidatesLabel_;
 
   //variables which would become branches
+  Float_t rho2012_;
   Int_t nPho_;
-  std::vector<Float_t> photonPt_;
-  std::vector<Float_t> photonEta_;
-  std::vector<Float_t> photonPhi_;
+  std::vector<Float_t> phoEt_;
+  std::vector<Float_t> phoSCEta_;
+  std::vector<Float_t> phoPhi_;  
+  std::vector<Int_t>   phoEleVeto_;
+  std::vector<Float_t> phoSigmaIEtaIEta_;
+  std::vector<Float_t> phoHoverE12_;
+  std::vector<Float_t> phoPFChIso_;
+  std::vector<Float_t> phoPFNeuIso_;
+  std::vector<Float_t> phoPFPhoIso_;
+  std::vector<Float_t> phoR9_;
+  std::vector<Float_t> phoHoverE_;
+  std::vector<Float_t> phoHcalIsoDR03_;
+  std::vector<Float_t> phoTrkIsoHollowDR03_;
+  std::vector<std::vector<Float_t>> phoCiCPF4chgpfIso02_;
 
 };
 
