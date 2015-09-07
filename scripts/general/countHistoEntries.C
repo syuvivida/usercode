7a31101537bee1d3b146d21cd8cc4f15e88e8cdb
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-09-07 12:32:32 +0200

    update so that subfolders could be read

--- scripts/general/countHistoEntries.C
+++ scripts/general/countHistoEntries.C
@@ -10,6 +10,7 @@
 #include <TList.h>
 using namespace std;
 
+
 void countHistoEntries(std::string dirname, std::string rootname="NCU")
 {
 
@@ -22,12 +23,28 @@ void countHistoEntries(std::string dirname, std::string rootname="NCU")
   Long64_t jEntry=0;
   while(fileH = (TFile*)fileIt()) {
     std::string fileN = fileH->GetName();
-    std::string baseString = rootname.data();
-    if( fileH->IsFolder())  continue;
-    if(fileN.find(baseString) == std::string::npos)continue;
-    TFile *inf = new TFile(Form("%s/%s",dirname.data(),fileN.data()));
-    TH1F *h = (TH1F*)inf->FindObjectAny("totalEvents");
-    jEntry += h->GetEntries();
+    std::string baseString = rootname.data(); 
+    // cout << fileN << endl;
+    if(fileN.find("fail") != std::string::npos)continue;
+    if(fileH->IsFolder()){
+     std::string newDir=dirname+fileN;
+      base->SetDirectory(newDir.data());
+      TList *listOfFiles2 = base->GetListOfFiles();
+      TIter fileIt2(listOfFiles2);
+      TFile *fileH2 = new TFile();  
+      while(fileH2 = (TFile*)fileIt2()) {
+	std::string fileN2 = fileH2->GetName();
+	// cout << fileN2 << endl;
+	if(fileH2->IsFolder())continue;
+ 	if(fileN2.find("fail") != std::string::npos)continue;
+ 	if(fileN2.find(baseString) == std::string::npos)continue;
+	// cout << fileN2.data() << endl;
+	TFile *inf = TFile::Open(Form("%s/%s",newDir.data(),fileN2.data()));
+	TH1F *h = (TH1F*)inf->FindObjectAny("totalEvents");
+	jEntry += h->GetEntries();
+	inf->Close();
+      }
+    } 
 
   }
 
@@ -35,3 +52,4 @@ void countHistoEntries(std::string dirname, std::string rootname="NCU")
 
 
 }
+
