4aeba6196ef989f1bcae3c44b4a861f17988c850
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-10-19 12:25:03 +0200

    updated so that it could be used for folders with many layers of subdirectories

--- scripts/general/countHistoEntries.C
+++ scripts/general/countHistoEntries.C
@@ -5,49 +5,52 @@
 #include "TKey.h"
 #include <string>
 #include <iostream>
+#include <fstream>
 #include <TSystem.h>
 #include <TSystemDirectory.h>
 #include <TList.h>
 using namespace std;
 
-
-void countHistoEntries(std::string dirname, std::string rootname="NCU")
+void countHistoEntries(string dirname, string rootname="NCU", string id="data7")
 {
-
-  TSystemDirectory *base = new TSystemDirectory("root","root");
-
-  base->SetDirectory(dirname.data());
-  TList *listOfFiles = base->GetListOfFiles();
-  TIter fileIt(listOfFiles);
-  TFile *fileH = new TFile();
+  string inputTextFile = "inputdir.txt";
+  // clean-up files
+  gSystem->Exec(Form("rm -rf %s",inputTextFile.data()));
+  // output new text file
+  gSystem->Exec(Form("ls -R %s | grep -a \"%s\" >> %s",
+		     dirname.data(),id.data(), inputTextFile.data()));
+  TSystemDirectory *base = new TSystemDirectory("root","root"); 
   Long64_t jEntry=0;
-  while(fileH = (TFile*)fileIt()) {
-    std::string fileN = fileH->GetName();
-    std::string baseString = rootname.data(); 
-    // cout << fileN << endl;
-    if(fileN.find("fail") != std::string::npos)continue;
-    if(fileH->IsFolder()){
-     std::string newDir=dirname+fileN;
-      base->SetDirectory(newDir.data());
-      TList *listOfFiles2 = base->GetListOfFiles();
-      TIter fileIt2(listOfFiles2);
-      TFile *fileH2 = new TFile();  
-      while(fileH2 = (TFile*)fileIt2()) {
-	std::string fileN2 = fileH2->GetName();
-	// cout << fileN2 << endl;
-	if(fileH2->IsFolder())continue;
- 	if(fileN2.find("fail") != std::string::npos)continue;
- 	if(fileN2.find(baseString) == std::string::npos)continue;
-	// cout << fileN2.data() << endl;
-	TFile *inf = TFile::Open(Form("%s/%s",newDir.data(),fileN2.data()));
-	TH1F *h = (TH1F*)inf->FindObjectAny("totalEvents");
-	jEntry += h->GetEntries();
-	inf->Close();
-      }
-    } 
+  int nfile=0;
+  string tempdir;
+  ifstream fin;
+  fin.open(inputTextFile.data());
+  fin >> tempdir;
+  TString realDirName = gSystem->GetFromPipe(Form("a=%s; echo ${a%%:*}",tempdir.data()));
+  while(!fin.eof()){
+    cout << "Directory name = " << realDirName << endl;
+    base->SetDirectory(realDirName.Data());
+    TList *listOfFiles = base->GetListOfFiles();
+    TIter fileIt(listOfFiles);
+    TFile *fileH = new TFile();
+    while(fileH = (TFile*)fileIt()) {
+      std::string fileN = fileH->GetName();
+      std::string baseString = rootname.data();
+      if( fileH->IsFolder())  continue;
+      if(fileN.find(baseString) == std::string::npos)continue;
+      cout << fileN.data() << endl;
+      nfile++;
+      TFile *inf = TFile::Open(Form("%s/%s",realDirName.Data(),fileN.data()));
+      TH1F *h = (TH1F*)inf->FindObjectAny("totalEvents");
+      jEntry += h->GetEntries();
+      inf->Close();
+    }
+    fin >> tempdir;
+    realDirName = gSystem->GetFromPipe(Form("a=%s; echo ${a%%:*}",tempdir.data()));
+  } // end of big loop
 
-  }
-
+  std::cout << "Opened " << nfile << " files" << std::endl;
+  
   cout << "Total number of entries = " << jEntry << endl;
 
 
